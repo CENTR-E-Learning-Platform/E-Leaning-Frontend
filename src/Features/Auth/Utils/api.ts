7c8d67cfdd490000d1cc8f585d733d8e21a9892d
abdoo-ahmed <abdoahmed8959@gmail.com>
2025-10-24 00:17:25 +0300

    Create External Facebook and fix some problems

--- src/Features/Auth/Utils/api.ts
+++ src/Features/Auth/Utils/api.ts
@@ -1,6 +1,5 @@
 const LOGIN_API  = "https://runnier-uncongealable-samuel.ngrok-free.dev/Account/Login";
 const REGISTER_API = "https://runnier-uncongealable-samuel.ngrok-free.dev/Account/Register";
-const EXTERNAL_LOGIN_API = "https://runnier-uncongealable-samuel.ngrok-free.dev/external-login";
+const EXTERNAL_LOGIN_API = "https://localhost:7251/Account/external-login";
 
 export {LOGIN_API , REGISTER_API , EXTERNAL_LOGIN_API}
-
