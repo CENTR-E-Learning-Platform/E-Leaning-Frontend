fb3e2acf5fe74ad6de150caf4972c6d0d2d67073
abdoo-ahmed <abdoahmed8959@gmail.com>
2025-10-30 21:48:43 +0300

    Update Routes Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import { router } from './Routes/Routes'
-import { RegProvider } from './Features/Auth/Contexts/RegContext'
-import {LoginProvider} from './Features/Auth/Contexts/LoginContext';
-
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import { router } from "./Routes/Routes";
+import { RegProvider } from "./Features/Auth/Contexts/RegContext";
+import { LoginProvider } from "./Features/Auth/Contexts/LoginContext";
 
 function App() {
-
   return (
     <>
       <LoginProvider>
         <RegProvider>
-        <RouterProvider router={router}/>
-      </RegProvider>
+          <RouterProvider router={router} />
+        </RegProvider>
       </LoginProvider>
-      
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -4,13 +4,23 @@ import MainLogin from "../Features/Auth/Pages/MainLogin";
 import OptionRegister from "../Features/Auth/Components/Register/OptionRegister";
 import TeacherOption from "../Features/Auth/Components/Register/TeacherOption";
 import StudentOption from "../Features/Auth/Components/Register/StudentOption";
+import OTP from "../Features/Auth/Components/Log in/OTP";
+import ForgetPassword from "../Features/Auth/Components/Log in/ForgetPassword";
+import SetNewPassword from "../Features/Auth/Components/Log in/SetNewPassword";
+import EmailConfig from "../Features/Auth/Components/Register/EmailConfig";
 
-export const router =createBrowserRouter([
-  {path:'/',element:<MainRegister/>},
-  {path:'/register',element:<MainRegister/>},
-  {path:'/login',element:<MainLogin/>},
-  {path:'/OptionRegister',element:<OptionRegister/>},
-  {path:'/TeacherOption',element:<TeacherOption/>},
-  {path:'/StudentOption',element:<StudentOption/>},
-  
-])
+export const router = createBrowserRouter([
+  { path: "/", element: <MainRegister /> },
+  { path: "register", element: <MainRegister /> },
+  { path: "login", element: <MainLogin /> },
+  { path: "login/otp", element: <OTP /> },
+
+  { path: "login/SendEmail", element: <ForgetPassword /> },
+  { path: "login/SendEmail/otp", element: <OTP /> },
+  { path: "login/SendEmail/otp/setNewPassword", element: <SetNewPassword /> },
+
+  { path: "OptionRegister", element: <OptionRegister /> },
+  { path: "TeacherOption", element: <TeacherOption /> },
+  { path: "StudentOption", element: <StudentOption /> },
+  { path: "/confing", element: <EmailConfig /> },
+]);
