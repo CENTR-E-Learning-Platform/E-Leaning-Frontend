8e2096c221e1007a1b0b10cb22e14b9a66774f74
Abdelrhmancs <ght7234@gmail.com>
2025-11-25 01:50:55 +0200

    create Hook of payment

---
+++ src/Features/Payment/Hooks/useInitialAmount.ts
@@ -0,0 +1,75 @@
+import { amountServices } from "../Services/amount";
+import { paymob } from "../Services/paymob";
+import { sendcharge } from "../Services/charge";
+import { paymobile } from "../Services/payMobile";
+export const useInitialAmount = ()=> {
+    const amount:number = 100; 
+    const sendAmount =async () => {
+        try{
+            const response = await amountServices(amount);
+            //console.log("ptimary key", response)
+            localStorage.setItem("paymentKey", response.data.paymentKey);
+            
+            console.log(response.data.paymentKey);
+                     
+        }catch(err){
+            console.log("exceptions "+ err);  
+            alert(err);     
+        }        
+    }
+    
+    const sendpaymob =async(userDetails : object)=>{
+        try{
+            const response = await paymob(userDetails);
+            console.log(response.data);
+            localStorage.setItem("masked_pan", response.data.masked_pan);
+            localStorage.setItem("tokenpayment", response.data.token);
+        } catch(err){
+            console.log("try catch", err);
+        }
+    }
+
+    const sendingcharge = async()=> {
+        const chargeData =   
+                {
+                    paymentKey : localStorage.getItem("paymentKey")?.toString(),
+                    token : localStorage.getItem("tokenpayment")?.toString(),
+                    masked_pan : localStorage.getItem("masked_pan")?.toString() 
+                }
+                //const jsonPayload = JSON.parse(JSON.stringify(chargeData));
+                console.log(chargeData);
+            try{
+                const response = await sendcharge(
+                    {
+                        "paymentKey":chargeData.paymentKey,
+                        "token" : chargeData.token,
+                        "maskedPan" : chargeData.masked_pan
+                    }
+                )
+               // window.location.href = response.data.redirection_url;
+               const url = response.data.details.redirection_url;
+               window.open(url, "_blank");
+                console.log(response.data);
+                
+            }catch(err){
+                console.log(err);
+                
+            }
+    }
+
+    const sendWallet = async(data:object) => {
+        try{
+             
+        const response =await paymobile(data);
+        console.log( response.data);
+        
+        const url = response.data.redirectUrl;
+        window.open(url, "_blank");
+        
+        }catch(err){
+            console.log(err);
+            
+        }
+    }
+    return {sendAmount , sendpaymob, sendingcharge , sendWallet}
+}
