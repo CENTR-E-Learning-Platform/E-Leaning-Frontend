bf5436cedffb68a0b336ec4bc8471573cc5f9679
Abdelrhmancs <ght7234@gmail.com>
2025-12-09 02:47:13 +0200

    create controlcontext

---
+++ src/Features/Streaming/Context/ControlContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext , useContext, useState , type ReactNode  } from "react"
+
+interface RoomContextTypes {
+  cameraView: boolean;
+  setCameraView: React.Dispatch<React.SetStateAction<boolean>>;
+  mic: boolean;
+  setMic: React.Dispatch<React.SetStateAction<boolean>>;
+  openStream: boolean;
+  setOpenStream: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const RoomCtx = createContext<RoomContextTypes | null>(null);
+
+export const RoomProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [cameraView, setCameraView] = useState(true);
+  const [mic, setMic] = useState(true);
+  const [openStream, setOpenStream] = useState(false);
+  return (
+    <RoomCtx.Provider value={{ cameraView, setCameraView , mic , setMic , openStream , setOpenStream}}>
+      {children}
+    </RoomCtx.Provider>
+  );
+};
+
+export const useControlContext = (): RoomContextTypes => {
+  const ctx = useContext(RoomCtx);
+  if (!ctx) {
+    throw new Error("RoomContext must be used within RoomProvider");
+  }
+  return ctx; 
+};
