8e7715afced3db0b35cb10b2a76297586c2f8150
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-01-26 01:36:57 +0200

    Add Explore Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
+import "./App.css";
+import { RoomProvider } from "./Features/Streaming/Context/ControlContext";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./Routes/Routes";
 import { RegProvider } from "./Features/Auth/Contexts/RegContext";
 import { LoginProvider } from "./Features/Auth/Contexts/LoginContext";
-import { RoomProvider } from "./Features/Streaming/Context/ControlContext";
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 function App() {
+
+  const queryClient = new QueryClient()
   return (
     <>
-   
-       <LoginProvider>
-        <RegProvider>
-           <RoomProvider>
-          <RouterProvider router={router} />
-          </RoomProvider>
-        </RegProvider>
-      </LoginProvider>
-    
-     
+      <QueryClientProvider client={queryClient}> 
+        <LoginProvider>
+          <RegProvider>
+            <RoomProvider>
+            <RouterProvider router={router} />
+            </RoomProvider>
+          </RegProvider>
+        </LoginProvider>
+      </QueryClientProvider>
     </>
   );
 }
