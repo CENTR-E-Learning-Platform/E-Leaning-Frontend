75322a392a94856cf2673cd9e4c3a2d6a1253ce1
Abdelrhmancs <ght7234@gmail.com>
2026-02-08 19:43:58 +0200

    create Grad Button

---
+++ src/Features/Calendar/Components/Form/Grad.tsx
@@ -0,0 +1,13 @@
+const Grad = ({ title }: any) => {
+  return (
+    <>
+      <input
+        type="button"
+        value={title}
+        className="w-[143px] h-[42px] border-[2px] bg-[#FFFFFF] border-[#D1D5DB] rounded-[8px] focus:outline-none p-[8px]  text-[15px] font-medium cursor-pointer"
+      />
+    </>
+  );
+};
+
+export default Grad;
