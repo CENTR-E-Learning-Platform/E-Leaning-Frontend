a03c4ff78194c79bd854f78c4076c11b3f25fb37
Abdelrhmancs <ght7234@gmail.com>
2026-02-08 19:43:24 +0200

    create Add Class Button

---
+++ src/Features/Calendar/Components/Form/Button.tsx
@@ -0,0 +1,13 @@
+const Button = ({ title, bg, txt }: any) => {
+  return (
+    <>
+      <input
+        type="submit"
+        value={title}
+        className={`w-[220px] h-[44px] rounded-[8px] bg-[${bg}] text-[${txt}] text-[16px] font-semibold mt-[24px] mb-[15px] cursor-pointer border-[1px] border-[#525FE1]`}
+      />
+    </>
+  );
+};
+
+export default Button;
