6f858f941074e0509104cb8cf58e35f2817bf669
Abdelrhmancs <ght7234@gmail.com>
2026-02-08 19:42:58 +0200

    create classButton

--- src/Features/Calendar/Components/ClassButton.tsx
+++ src/Features/Calendar/Components/ClassButton.tsx
@@ -1,12 +1,11 @@
-
 const ClassButton = () => {
   return (
-   <>
-        <button className="w-[259px] h-[44px] rounded-[8px] bg-[#525FE1] text-[#F9FBFC] text-[16px] font-semibold mt-[24px] mb-[15px] cursor-pointer">
-            Add new class
-        </button>
-   </>
-  )
-}
+    <>
+      <button className="w-[259px] h-[44px] rounded-[8px] bg-[#525FE1] text-[#F9FBFC] text-[16px] font-semibold mt-[24px] mb-[15px] cursor-pointer">
+        Add new class
+      </button>
+    </>
+  );
+};
 
 export default ClassButton;
