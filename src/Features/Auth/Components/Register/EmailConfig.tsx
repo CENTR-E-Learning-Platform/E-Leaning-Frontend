75cd7cb068018532f7591b0851396dcd588e9fb4
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-02-10 04:05:11 +0200

    Update Email Config Page

--- src/Features/Auth/Components/Register/EmailConfig.tsx
+++ src/Features/Auth/Components/Register/EmailConfig.tsx
@@ -2,8 +2,23 @@ import React from "react";
 
 import email_Config from "../../../../assets/images/EmailConfig.png";
 import { useRegContext } from "../../Contexts/RegContext";
+import { Link } from "react-router";
+import { useConfirmEmail } from "../../Hooks/useConfirmEmail";
 const EmailConfig = () => {
   const { userData } = useRegContext();
+  const { refetch , data , error , isLoading } = useConfirmEmail(userData?.email || "");
+
+  if (error) {
+    console.log(error.message);
+  }
+  if (data) {
+    console.log(data.data);
+  }
+
+  const handleResend = () => {
+    refetch();
+  };
+
   return (
     <React.Fragment>
       <main className="w-screen h-screen">
@@ -16,18 +31,18 @@ const EmailConfig = () => {
                 </h1>
               </header>
 
-              <article className="flex flex-col  items-center justify-center">
+              <article className="flex flex-col w-[640px] items-start justify-center">
                 <h2 className="text-[29px] mt-[160px] font-bold leading-[20.390625px] text-[#2A2D34]">
                   Confirm your email to continue
                 </h2>
-
-                <section className="h-[90px] mt-[20px] mb-[10px]">
-                  <p className="text-[17px] w-[425px] leading-[155%] font-normal text-gray-900 mb-1">
+                
+                <section className="h-[90px] w-[640px] mt-[20px] mb-[10px]">
+                  <p className="text-[17px] leading-[155%] font-normal text-gray-900 mb-1">
                     We’ve sent an email to {userData?.email} <br />
                     Confirm this is correct by clicking the link in the email.
                   </p>
 
-                  <p className="text-[12.7px] w-[425px] leading-[155%] font-normal">
+                  <p className="text-[12.7px] leading-[155%] font-normal">
                     Didn’t get an email? Check your spam folder or request
                     another email.
                   </p>
@@ -35,17 +50,20 @@ const EmailConfig = () => {
 
                 <div className="flex items-center w-[425px] gap-5 justify-start">
                   <button
+                    onClick={handleResend}
+                    disabled={isLoading}
                     type="button"
-                    className="w-[220.8px] font-medium cursor-pointer h-[36.6px] bg-[#525FE1] text-[#fff] border border-[#525FE1] rounded-[8px]"
+                    className="w-[230px] p-1 font-medium cursor-pointer h-[36.6px] bg-[#525FE1] text-[#fff] border border-[#525FE1] rounded-[8px]"
                   >
-                    Resend confirmation email
+                    {isLoading ? "Sending..." : "Resend confirmation email"}
+
                   </button>
-                  <button
-                    type="button"
-                    className="w-[178.6px] cursor-pointer h-[36.6px] text-[#525FE1] border border-[#525FE1] rounded-[8px]"
+                  <Link
+                    to = "/register"
+                    className="w-[178.6px] flex items-center justify-center cursor-pointer h-[36.6px] text-[#525FE1] border border-[#525FE1] rounded-[8px]"
                   >
                     Create new account
-                  </button>
+                  </Link>
                 </div>
               </article>
             </div>
