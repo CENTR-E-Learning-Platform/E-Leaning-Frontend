c3a10711983f9efdb4398704a2d2cc73027d4505
Abdelrhmancs <ght7234@gmail.com>
2026-02-16 22:04:04 +0200

    try to fix service

--- src/Features/Streaming/Services/createRoom.ts
+++ src/Features/Streaming/Services/createRoom.ts
@@ -4,14 +4,13 @@ import axios from "axios";
 export const createRoom = async (data: any) => {
   const token = localStorage.getItem("token");
   console.log("this is token", token);
+  console.log(data.params);
+  
 
-  return await axios.post(
-    `${CREATEROOM_API}?${data}`,
-    {}, 
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
+  return await axios.post(CREATEROOM_API, {}, { 
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    params: data 
+  });
 };
