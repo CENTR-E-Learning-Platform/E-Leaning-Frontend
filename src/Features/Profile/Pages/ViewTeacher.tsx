f0cd678bd4f4344e5ab401c47c42c786c9bb81ac
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-02-17 00:47:35 +0200

    Adding Main View Teacher

---
+++ src/Features/Profile/Pages/ViewTeacher.tsx
@@ -0,0 +1,16 @@
+import ProfileHeader from "../Components/ViewTeacher/ProfileHeader"
+
+const ViewTeacher = () => {
+  return <>
+
+    <section className="ProfileStudent-section">
+
+      <ProfileHeader/>
+
+    </section>
+  
+  
+  </>
+}
+
+export default ViewTeacher
