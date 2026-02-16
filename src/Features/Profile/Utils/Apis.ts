373a4fd471d9e9047f505803afba10758de8ee9e
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-02-17 00:43:01 +0200

    Adding API Profile

---
+++ src/Features/Profile/Utils/Apis.ts
@@ -0,0 +1,5 @@
+const BASE_URL = "https://localhost:7251";
+const UploadImage_API = `${BASE_URL}/api/TeacherProfile/Add%20Profile%20Picture`;
+const TeacherProfile_API = `${BASE_URL}/api/TeacherProfile/Get%20TeacherProfile%20Data`;
+
+export {UploadImage_API ,TeacherProfile_API }
