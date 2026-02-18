faea2324578a96c20d2b978515262dcba4adee1b
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-02-18 05:03:31 +0200

    Change Path APIs

--- src/Features/Profile/Utils/Apis.ts
+++ src/Features/Profile/Utils/Apis.ts
@@ -1,5 +1,5 @@
 const BASE_URL = "https://localhost:7251";
-const UploadImage_API = `${BASE_URL}/api/TeacherProfile/Add%20Profile%20Picture`;
-const TeacherProfile_API = `${BASE_URL}/api/TeacherProfile/Get%20TeacherProfile%20Data`;
+const UPLOADIMAGE_API = `${BASE_URL}/api/TeacherProfile/Add%20Image`;
+const TEACHERPROFILE_API = `${BASE_URL}/api/TeacherProfile/Get%20TeacherProfile%20Data`;
 
-export {UploadImage_API ,TeacherProfile_API }
+export {UPLOADIMAGE_API ,TEACHERPROFILE_API }
