33294c8c8a137654352065c2b266aa83838f98c7
Abdelrhmancs <ght7234@gmail.com>
2026-02-19 04:18:24 +0200

    add options to context

--- src/Features/Streaming/Context/ControlContext.tsx
+++ src/Features/Streaming/Context/ControlContext.tsx
@@ -7,6 +7,20 @@ interface RoomContextTypes {
   setMic: React.Dispatch<React.SetStateAction<boolean>>;
   openStream: boolean;
   setOpenStream: React.Dispatch<React.SetStateAction<boolean>>;
+  optionMic: boolean;
+  setOptionMic: React.Dispatch<React.SetStateAction<boolean>>;
+  optionCamera: boolean;
+  setOptionCamera: React.Dispatch<React.SetStateAction<boolean>>;
+  optionMenu: boolean;
+  setOptionMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  optionSmallMenu: boolean;
+  setOptionSmallMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  optionEmoji: boolean;
+  setOptionEmoji: React.Dispatch<React.SetStateAction<boolean>>;
+  emoji: string [];
+  setEmoji: React.Dispatch<React.SetStateAction<string[]>>;
+  optionLeave: boolean;
+  setOptionLeave: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const RoomCtx = createContext<RoomContextTypes | null>(null);
@@ -15,8 +29,15 @@ export const RoomProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   const [cameraView, setCameraView] = useState(false);
   const [mic, setMic] = useState(false);
   const [openStream, setOpenStream] = useState(false);
+  const [optionMic, setOptionMic] = useState(false);
+  const [optionCamera, setOptionCamera] = useState(false);
+  const [optionMenu, setOptionMenu] = useState(false);
+  const [optionSmallMenu, setOptionSmallMenu] = useState(false);
+  const [optionEmoji, setOptionEmoji] = useState(false);
+  const [emoji, setEmoji] = useState<string[]>([]);
+  const [optionLeave, setOptionLeave] = useState(false)
   return (
-    <RoomCtx.Provider value={{ cameraView, setCameraView , mic , setMic , openStream , setOpenStream}}>
+    <RoomCtx.Provider value={{ cameraView, setCameraView , mic , setMic , openStream , setOpenStream , optionMic , setOptionMic ,optionCamera , setOptionCamera , optionMenu , setOptionMenu , optionSmallMenu , setOptionSmallMenu , optionEmoji , setOptionEmoji ,emoji ,setEmoji , optionLeave , setOptionLeave}}>
       {children}
     </RoomCtx.Provider>
   );
