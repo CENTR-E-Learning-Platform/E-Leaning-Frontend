cf5b991035e3baddb5b1d98b8ac69ab4e56cac27
Abdelrhmancs <ght7234@gmail.com>
2026-02-20 05:05:07 +0200

    add props

--- src/Features/Streaming/Components/meeting/Button.tsx
+++ src/Features/Streaming/Components/meeting/Button.tsx
@@ -3,7 +3,7 @@ const Button = (props:any) => {
         <>
             <button 
             onClick={ props.func}
-            className="bg-[#2A2D34] w-[48px] h-[48px] rounded-[8px] flex justify-center items-center cursor-pointer me-[4px] border-[2px] border-[#393D44] hover:bg-[#454950] transition duration-300">
+            className={`bg-[#2A2D34] ${props.customStyle}  w-[48px] h-[48px] rounded-[8px] flex justify-center items-center cursor-pointer me-[4px] border-[2px] border-[#393D44] hover:bg-[#454950] transition duration-300`}>
                 <img src={props.icons} className={props.size} alt="" />
             </button>
         </>
