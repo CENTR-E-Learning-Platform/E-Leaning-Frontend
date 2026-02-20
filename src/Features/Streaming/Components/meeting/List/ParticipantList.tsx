eea11ac0b1479ad86a595802f7e6ca50da0a687a
Abdelrhmancs <ght7234@gmail.com>
2026-02-20 23:19:31 +0200

    add func to mute

--- src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
+++ src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
@@ -3,11 +3,16 @@ import mic from '../../../../../assets/icons/mic2.svg';
 import disMic from '../../../../../assets/icons/disableMic.svg';
 import disrais from '../../../../../assets/icons/disablehand.svg';
 import kick from '../../../../../assets/icons/kick.svg';
-
+import { useRole } from "../../../Hooks/useRole";
 const participantList = () => {
+  const {MuteParticipant} = useRole();
   return (
     <>
-        <ParticipantComp rad = "rounded-t-[8px]" icon ={mic} color = "#F9FBFC" parg ="Mute Participant" />     
+        <ParticipantComp 
+        func = {()=> {
+          MuteParticipant();
+        }}
+        rad = "rounded-t-[8px]" icon ={mic} color = "#F9FBFC" parg ="Mute Participant" />     
         <ParticipantComp rad = "" icon ={disMic}  color = "#F9FBFC" parg ="Disable Mic" />     
         <ParticipantComp rad = "" icon ={disrais} color = "#F9FBFC" parg ="Lower Hand" />     
         <ParticipantComp rad = "rounded-b-[8px]" icon ={kick} color = "#D24747" parg ="Remove Participant" />     
