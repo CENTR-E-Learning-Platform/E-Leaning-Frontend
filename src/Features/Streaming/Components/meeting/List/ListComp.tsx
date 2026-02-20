10c4ba8b952b89f3ed3c210e55bd682e27cce894
Abdelrhmancs <ght7234@gmail.com>
2026-02-20 23:14:50 +0200

    fix design

--- src/Features/Streaming/Components/meeting/List/ListComp.tsx
+++ src/Features/Streaming/Components/meeting/List/ListComp.tsx
@@ -3,7 +3,7 @@
 const ListComp = ({width , icon , parg ,height , arrow , visable}:any) => {
   return (
     <>
-        <div className={`${width} ${height} bg-[#454950] rounded-[8px] flex justify-between items-center px-[10px] mb-[0.5px] cursor-pointer`}>
+        <div className={`${width} ${height} bg-[#454950] flex justify-between items-center px-[10px] mb-[0.5px] cursor-pointer hover:bg-[#4f5257] transition duration-300`}>
             <div className='flex justify-center items-center'>
                 <img src={icon} className='w-[16px] h-[18px]' alt="" />
             <h1 className='text-[#F9FBFC] ms-[8px] text-[14px] font-medium'>{parg}</h1>
