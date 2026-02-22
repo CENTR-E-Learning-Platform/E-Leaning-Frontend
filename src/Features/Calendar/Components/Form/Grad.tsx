c6d3586feca48d2b585f71ec86764b541097f622
Abdelrhmancs <ght7234@gmail.com>
2026-02-22 05:07:14 +0200

    fix clicked and get value

--- src/Features/Calendar/Components/Form/Grad.tsx
+++ src/Features/Calendar/Components/Form/Grad.tsx
@@ -1,10 +1,11 @@
-const Grad = ({ title }: any) => {
+const Grad = ({ title , change  ,checked}: any) => {
   return (
     <>
       <input
+        onClick={change}
         type="button"
         value={title}
-        className="w-[143px] h-[42px] border-[2px] bg-[#FFFFFF] border-[#D1D5DB] rounded-[8px] focus:outline-none p-[8px]  text-[15px] font-medium cursor-pointer"
+        className={`w-[143px] ${checked ? "bg-[#a79d9d]" : ""} h-[42px] border-[2px] bg-[#FFFFFF] border-[#D1D5DB] rounded-[8px] focus:outline-none p-[8px]  text-[15px] font-medium cursor-pointer`}
       />
     </>
   );
