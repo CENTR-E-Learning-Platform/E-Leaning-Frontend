95ba44b5c1d731335ab537e3bbded8c86a864045
Abdelrhmancs <ght7234@gmail.com>
2026-02-22 05:11:13 +0200

    fix api

--- src/Features/Streaming/Services/joinRoom.ts
+++ src/Features/Streaming/Services/joinRoom.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { BASE_URL } from "../Utils/Apis"
 export const joinRoom = async()=> {
-    const roomname = localStorage.getItem("sessionName");
+    const roomname = localStorage.getItem("sessionName")?.toString();
+    console.log(roomname);
+    
     const token = localStorage.getItem("token");
     return await axios.get(
-        `${BASE_URL}/${roomname}/token`,
+        `${BASE_URL}/api/Room/${roomname}/token`,
         {
             headers: {
         Authorization: `Bearer ${token}`
