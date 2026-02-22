f5d3d259b60273ad18241fd427473748cc2b1912
Abdelrhmancs <ght7234@gmail.com>
2026-02-22 05:07:46 +0200

    fix props

--- src/Features/Calendar/Components/Form/MaxNoOfStudent.tsx
+++ src/Features/Calendar/Components/Form/MaxNoOfStudent.tsx
@@ -1,18 +1,15 @@
-const MaxNoOfStudent = () => {
+const MaxNoOfStudent = ({change , val}:any) => {
   return (
     <>
       <div className="flex flex-col w-[449px] ">
         <label htmlFor=" " className="text-[15px] font-[400] text-[#2A2D34]">
-          Maximum number of students
+          Price
         </label>
         <input
+        name="Price"
           type="number"
-          onChange={(e: any) => {
-            if (e.key === "-") e.preventDefault();
-            if (e.target.value > 50) {
-              e.target.value = 50;
-            }
-          }}
+          onChange={change}
+          value={val}
           placeholder="none"
           className="w-[244px] h-[42px] border-[2px] bg-[#FFFFFF] border-[#D1D5DB] rounded-[8px] focus:outline-none pl-[16px] text-[#2A2D34] text-[16px] font-[400] me-[10px] [appearance:textfield]
   [&::-webkit-outer-spin-button]:appearance-none
