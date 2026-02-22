1e39b5e5b3fe65ac467962108246f6f8ec37ab57
Abdelrhmancs <ght7234@gmail.com>
2026-02-22 05:06:49 +0200

    add props to handle changes and set value

--- src/Features/Calendar/Components/Form/Description.tsx
+++ src/Features/Calendar/Components/Form/Description.tsx
@@ -1,11 +1,15 @@
-const Description = () => {
+const Description = ({change , val}:any) => {
   return (
     <>
       <div className="flex flex-col w-[449px]">
-        <label htmlFor=" " className="text-[16px] font-[400] text-[#2A2D34]">
+        <label 
+        htmlFor=" " className="text-[16px] font-[400] text-[#2A2D34]">
           Description
         </label>
         <textarea
+        name="Description"
+        onChange={change}
+        value={val}
           placeholder="“What will students learn in this class?"
           className="w-[449px] h-[120px] border-[2px] bg-[#FFFFFF] border-[#D1D5DB] rounded-[8px] focus:outline-none p-[8px]  text-[15px] font-[400] me-[10px] placeholder:text-[#6D7588]"
         ></textarea>
