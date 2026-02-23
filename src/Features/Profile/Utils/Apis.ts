6dbf8e16eb9561918770e7cefd550d08b0e486a2
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-02-23 07:33:32 +0200

    Adding New APIs

--- src/Features/Profile/Utils/Apis.ts
+++ src/Features/Profile/Utils/Apis.ts
@@ -1,5 +1,7 @@
 const BASE_URL = "https://localhost:7251";
 const UPLOADIMAGE_API = `${BASE_URL}/api/TeacherProfile/Add%20Image`;
 const TEACHERPROFILE_API = `${BASE_URL}/api/TeacherProfile/Get%20TeacherProfile%20Data`;
+const UPLOAD_INTRO_VIDEO_API = `${BASE_URL}/api/TeacherProfile/Upload%20Intro%20Video`;
+const STUDENTPROFILE_API = `${BASE_URL}/api/StudentProfile/Get%20StudentProfile%20Data`; // Setting feature
 
-export {UPLOADIMAGE_API ,TEACHERPROFILE_API }
+export {UPLOADIMAGE_API ,TEACHERPROFILE_API  , UPLOAD_INTRO_VIDEO_API  , STUDENTPROFILE_API}
