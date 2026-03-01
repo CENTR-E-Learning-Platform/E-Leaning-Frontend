d3b594ce0347c65593512cc8743b4d5bbc03d6d2
Abdelrhmancs <ght7234@gmail.com>
2026-03-01 02:59:48 +0200

    fix api of streaming feature

--- src/Features/Streaming/Utils/Apis.ts
+++ src/Features/Streaming/Utils/Apis.ts
@@ -1,4 +1,4 @@
 const BASE_URL = "https://localhost:7251";
-const CREATEROOM_API = `${BASE_URL}/api/Room/CreateRoom`;
+const CREATEROOM_API = `${BASE_URL}/api/Room/Create%20Room`;
 const CONTROL_API = `${BASE_URL}/ControlMeeting`;
 export {CREATEROOM_API , BASE_URL , CONTROL_API}
