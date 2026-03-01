aa8457040316db147f60a2379d546a9b25f620e4
Abdelrhmancs <ght7234@gmail.com>
2026-03-01 03:02:20 +0200

    fix design of student comp

--- src/Features/Streaming/Components/meeting/StudentActions.tsx
+++ src/Features/Streaming/Components/meeting/StudentActions.tsx
@@ -3,7 +3,7 @@ import menu from "../../../../assets/icons/smallmenu.svg";
 import raishand from "../../../../assets/icons/raishand.svg";
 import { useControlContext } from "../../Context/ControlContext";
 import ParticipantList from "./List/ParticipantList";
-
+import { BASE_URL } from "../../Utils/Apis";
 const StudentActions = ({ name, profileImage, width }: any) => {
   const {optionSmallMenu , setOptionSmallMenu} = useControlContext();
   return (
@@ -15,7 +15,7 @@ const StudentActions = ({ name, profileImage, width }: any) => {
       }
     >
       {width > 1133 && (
-        <div className="flex items-center  p-[4px]  ">
+        <div className="flex items-center p-[4px]">
           <img
             src={profileImage}
             className="w-[88px] h-[88px] rounded-full mb-[16px] "
@@ -28,13 +28,13 @@ const StudentActions = ({ name, profileImage, width }: any) => {
         <div className="flex items-center   ">
           {width < 1133 && (
             <img
-              src={profileImage}
-              className="w-[40px] h-[40px] rounded-full me-[13px]"
+              src={`${BASE_URL}/${profileImage}`}
+              className="w-[40px] h-[40px] rounded-full me-[13px] bg-cover"
               alt=""
             />
           )}
           {width < 1250 && (
-            <h1 className="text-[16px] text-[#F9FBFC] ">Mohamed</h1>
+            <h1 className="text-[16px] text-[#F9FBFC] w-[130px]">{name.length > 11 ? name.slice(0, 11) : name}</h1>
           )}
         </div>
       )}
