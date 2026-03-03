11b44275553232df496b98a5c81f323cddc19110
Abdelrhmancs <ght7234@gmail.com>
2026-03-03 22:26:58 +0200

    create hook  to Role

---
+++ src/Features/Streaming/Hooks/useRole.ts
@@ -0,0 +1,28 @@
+import { useRoomContext } from "@livekit/components-react";
+import {ControlParticipant} from "../Types/types";
+import { Role } from "../Services/Role";
+import { useControlContext } from "../Context/ControlContext";
+export const useRole = () => {
+    const room = useRoomContext();
+    const {setMic , setCameraView , setMute} = useControlContext();
+    const MuteParticipant = async (identity:any ) => {
+       try{
+       const roomName = localStorage.getItem("sessionName")?.toString();
+       ControlParticipant.identity = identity;
+       ControlParticipant.roomName = roomName ? roomName :"";
+       ControlParticipant.mute =  !ControlParticipant.mute;
+       console.log(ControlParticipant);
+       setCameraView(false);
+       setMic(false);
+       setMute((prev) => !prev);
+       const res =await Role(ControlParticipant);
+       console.log( res.data);
+       }catch(err:any){
+        console.log("errors" , err);
+        
+       }
+       
+    }
+    return {MuteParticipant}
+}
+
