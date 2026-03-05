de570a09c001b457ee40dd2a53b413c337017bbf
Abdelrhmancs <ght7234@gmail.com>
2026-03-06 00:01:25 +0200

    fix useRole

--- src/Features/Streaming/Hooks/useRole.ts
+++ src/Features/Streaming/Hooks/useRole.ts
@@ -3,23 +3,22 @@ import {ControlParticipant} from "../Types/types";
 import { Role } from "../Services/Role";
 import { useControlContext } from "../Context/ControlContext";
 export const useRole = () => {
-    const room = useRoomContext();
-    const {setMic , setCameraView , setMute} = useControlContext();
-    const MuteParticipant = async (identity:any ) => {
+   //  const room = useRoomContext();
+   //  const {setMic , setCameraView , setMute} = useControlContext();
+    const MuteParticipant = async (identity:any , isCurrentlyMicEnabled:any ) => {
        try{
        const roomName = localStorage.getItem("sessionName")?.toString();
        ControlParticipant.identity = identity;
        ControlParticipant.roomName = roomName ? roomName :"";
-       ControlParticipant.mute =  !ControlParticipant.mute;
+       ControlParticipant.mute =  isCurrentlyMicEnabled;
        console.log(ControlParticipant);
-       setCameraView(false);
-       setMic(false);
-       setMute((prev) => !prev);
+      //  setCameraView(false);
+      //  setMic(false);
+    //    setMute((prev) => !prev);
        const res =await Role(ControlParticipant);
        console.log( res.data);
        }catch(err:any){
         console.log("errors" , err);
-        
        }
        
     }
