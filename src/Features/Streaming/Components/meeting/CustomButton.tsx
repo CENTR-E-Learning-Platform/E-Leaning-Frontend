15f77320273a65865ef094561da19f40686ab1a1
Abdelrhmancs <ght7234@gmail.com>
2026-03-06 00:04:35 +0200

    fix state

--- src/Features/Streaming/Components/meeting/CustomButton.tsx
+++ src/Features/Streaming/Components/meeting/CustomButton.tsx
@@ -1,22 +1,23 @@
-import { useControlContext } from "../../Context/ControlContext"
+import { useLocalParticipant } from "@livekit/components-react";
 const CustomButton = (props:any) => {
-    const {mute} = useControlContext();
+    const {localParticipant} = useLocalParticipant();
+    const check = !localParticipant.permissions?.canPublish;
   return (
     <>
         <div className="">
             <button 
-            className={`${mute ? "opacity-60" : ""}  bg-[#454950] w-[71px] h-[48px] rounded-[8px] flex items-center cursor-pointer me-[4px] border-[2px] border-[#393D44]`}>
+            className={`${check ? "opacity-60" : ""}  bg-[#454950] w-[71px] h-[48px] rounded-[8px] flex items-center cursor-pointer me-[4px] border-[2px] border-[#393D44]`}>
                 <div 
-                onClick={!mute ? props.arrowFunc : undefined}
+                onClick={!check ? props.arrowFunc : undefined}
                 className="flex justify-center items-center w-[21px] h-full cursor-pointer">
                     <img src={props.arrow} className="w-[12px] h-[12px]" alt="" />
                 </div>
                 <div 
-                onClick={!mute ? props.func : undefined}
-                className={`w-[48px] h-[46px] ${mute ? "opacity-60" : ""}  bg-[#2A2D34] flex justify-center items-center rounded-[8px] cursor-pointer`}>
+                onClick={!check ? props.func : undefined}
+                className={`w-[48px] h-[46px] ${check ? "opacity-60" : ""}  bg-[#2A2D34] flex justify-center items-center rounded-[8px] cursor-pointer`}>
                     <img src={props.icons} className={props.size} alt="" />
                 </div>
-        </button>
+            </button>
         </div>
     </>
   )
