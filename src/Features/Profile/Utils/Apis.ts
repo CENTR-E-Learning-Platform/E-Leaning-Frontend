c0b419a85dace9ca009d0ac67d70f1434ed59382
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-03-07 21:41:31 +0200

    Add API

--- src/Features/Profile/Utils/Apis.ts
+++ src/Features/Profile/Utils/Apis.ts
@@ -3,6 +3,7 @@ const UPLOADIMAGE_API = `${BASE_URL}/api/TeacherProfile/Add%20Image`;
 const TEACHERPROFILE_API = `${BASE_URL}/api/TeacherProfile/Get%20TeacherProfile%20Data`;
 const UPLOAD_INTRO_VIDEO_API = `${BASE_URL}/api/TeacherProfile/Upload%20Intro%20Video`;
 const STUDENTPROFILE_API = `${BASE_URL}/api/StudentProfile/Get%20StudentProfile%20Data`; // Setting feature
-const DELETE_FILE_API = `${BASE_URL}/api/TeacherProfile/Delete%20File`
+const DELETE_FILE_API = `${BASE_URL}/api/TeacherProfile/Delete%20File`;
+const ADD_TEACHR_BIO_API = `${BASE_URL}/api/TeacherProfile/Add%20Teacher%20Bio`;
 
-export {UPLOADIMAGE_API ,TEACHERPROFILE_API  , UPLOAD_INTRO_VIDEO_API  , STUDENTPROFILE_API , DELETE_FILE_API}
+export {UPLOADIMAGE_API ,TEACHERPROFILE_API  , UPLOAD_INTRO_VIDEO_API  , STUDENTPROFILE_API , DELETE_FILE_API , ADD_TEACHR_BIO_API}
