1c7c245a5f5d9aba9307ba66f257074cc086cb9a
Abdelrhmancs <ght7234@gmail.com>
2026-03-15 05:46:47 +0200

    fix animation of raishand

--- src/Features/Streaming/Components/meeting/ParticipantsGrid.tsx
+++ src/Features/Streaming/Components/meeting/ParticipantsGrid.tsx
@@ -93,7 +93,7 @@ const ParticipantsGrid: React.FC<ParticipantsGridProps> = ({ isRais = [] }) => {
               initial={{ opacity: 0, y: 20, scale: 0.5 }}
               animate={{ opacity: 1, y: 0, scale: 1 }}
               exit={{ opacity: 0, y: 15, scale: 0.5 }}
-              transition={{ type: "spring", stiffness: 400, damping: 25 }}
+              transition={{ type: "spring", stiffness: 300, damping: 25 }}
               className={`flex items-center justify-center bg-[#1E1E1E] rounded-full ${circleSize}`}
             >
               <img src={raishand} alt="" className={iconSize} />
