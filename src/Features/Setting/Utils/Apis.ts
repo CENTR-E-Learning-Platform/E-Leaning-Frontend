fbc3c1bd059344e3e6f2b90e8ce558a6127f40f7
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-03-18 23:29:23 +0200

    Adding APIs

---
+++ src/Features/Setting/Utils/Apis.ts
@@ -0,0 +1,14 @@
+const BASE_URL = "https://localhost:7251";
+const GET_STUDENT_PROFILE_API = `${BASE_URL}/api/StudentProfile/Get_StudentProfile_Data`;
+const UPDATE_STUDENT_PROFILE_API = `${BASE_URL}/api/StudentProfile/Update_Studnet_Profile`;
+const DELETE_FILE_API = `${BASE_URL}/api/TeacherProfile/Delete%20File`;
+const UPLOADIMAGE_API = `${BASE_URL}/api/TeacherProfile/Add%20Image`;
+
+
+export{
+    GET_STUDENT_PROFILE_API,
+    UPDATE_STUDENT_PROFILE_API,
+    DELETE_FILE_API,
+    UPLOADIMAGE_API
+}
+
