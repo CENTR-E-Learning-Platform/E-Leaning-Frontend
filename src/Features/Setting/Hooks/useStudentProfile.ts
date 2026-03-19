3c7440f2968cfb1c9859f9c7507b9cf2068abdc0
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-03-19 23:35:04 +0200

    Adding Student Profile Hooks

---
+++ src/Features/Setting/Hooks/useStudentProfile.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { GetStudentProfileData } from "../Services/studentProfileAPI";
+
+export const useStudentProfile = () => {
+
+   const { data , refetch } = useQuery({
+        queryKey: ["StudentProfileData"],
+        queryFn: ()=>{
+            return GetStudentProfileData()
+        },
+        staleTime: Infinity,
+        gcTime: Infinity,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+    });
+
+    return {data , refetch};
+}
