660a134fca9ac124dd0f60adedbb9782c2b8cfaa
Abdelrhmancs <ght7234@gmail.com>
2026-03-21 16:26:42 +0200

    fix isRepeated to weeksNumber

--- src/Features/Streaming/Hooks/useCreateRoom.ts
+++ src/Features/Streaming/Hooks/useCreateRoom.ts
@@ -8,7 +8,7 @@ import { useControlContext } from "../Context/ControlContext";
 import { ClassFormShema } from "../Validation/ClassFormSchema";
 export const useCreateRoom = ()=> {
     const navigate = useNavigate();
-    const {initStream} = useControlling();
+    const {stopStream} = useControlling();
     const {setJoin} = useControlContext();
     const formik = useFormik({
         initialValues,
@@ -22,7 +22,7 @@ export const useCreateRoom = ()=> {
             Title: values.Title,
             StartTime: time.toISOString(),
             DurationMinutes: Number(values.DurationMinutes),
-            IsRepeat:values.IsRepeat,
+            weeksNumber:values.weeksNumber,
             ClassGrade: values.Grade,
             Reminder: values.Reminder,
             Description: values.Description || ""
@@ -34,8 +34,8 @@ export const useCreateRoom = ()=> {
             const response =await createRoom(params);
             console.log(response.data);
             console.log("success");
-            localStorage.setItem("sessionName" , response.data.sessionLiveKitRoom); 
-            navigate("/createroom/joinnow")
+            // localStorage.setItem("sessionName" , response.data.sessionLiveKitRoom); 
+            // navigate("/createroom/joinnow")
         }catch(err:any){
             console.log("Error Details:", err.response?.data);
             console.log(err);
@@ -47,13 +47,15 @@ export const useCreateRoom = ()=> {
     const JoinRoom = async () => {
         try{
             const roomname = localStorage.getItem("sessionName")?.toString();
-            const data = {roomName : roomname};
+            const data = {roomName :roomname};
             console.log(roomname);
             const response = await joinRoom(data);
             console.log(response.data);
             await localStorage.setItem("sessionToken", response.data.token);
             setJoin(true);
-             navigate("meeting")
+            // stopStream();
+            navigate("meeting")
+            
             // initStream();
         }catch(err){
             console.log(err);
