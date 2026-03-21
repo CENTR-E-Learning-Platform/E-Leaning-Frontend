a9ef399239d79c6529dc50b5320f5ab9eb137489
Abdelrhmancs <ght7234@gmail.com>
2026-03-21 16:18:48 +0200

    fix showing month

--- src/Features/Calendar/Components/Upcoming.tsx
+++ src/Features/Calendar/Components/Upcoming.tsx
@@ -1,16 +1,18 @@
-import UpcomingEvents from "./UpcomingEvents"
+import UpcomingEvents from "./UpcomingEvents";
+import { format } from "date-fns";
 
-
-const Upcoming =()=> {
+const Upcoming = ({ selectedDate }: { selectedDate: Date }) => {
   return (
-    <>
-        <div className="w-[265px] mt-[40px] ">
-            <h1 className="text-[18px] text-[#2A2D34] font-semibold">Upcoming events</h1>
-            <h1 className="text-[#6D7588] text-[16px] font-medium mt-[24px] mb-[16px]">October 27</h1>
-            <UpcomingEvents />
-        </div>
-    </>
-  )
-}
+    <div className="w-[265px] mt-[40px] ">
+      <h1 className="text-[18px] text-[#2A2D34] font-semibold">
+        Upcoming events
+      </h1>
+      <h1 className="text-[#6D7588] text-[16px] font-medium mt-[24px] mb-[16px]">
+        {format(selectedDate, "MMMM d")}
+      </h1>
+      <UpcomingEvents selectedDate={selectedDate} />
+    </div>
+  );
+};
 
-export default Upcoming
+export default Upcoming;
