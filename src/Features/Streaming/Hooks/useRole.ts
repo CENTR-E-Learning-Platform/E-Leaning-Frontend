e0941fe29190b7ced9d2c817437121b9a2713e3c
Abdelrhmancs <ght7234@gmail.com>
2026-03-24 05:23:15 +0200

    fix func name and add MuteParticipant func

--- src/Features/Streaming/Hooks/useRole.ts
+++ src/Features/Streaming/Hooks/useRole.ts
@@ -2,10 +2,13 @@ import { useRoomContext } from "@livekit/components-react";
 import {ControlParticipant} from "../Types/types";
 import { Role } from "../Services/Role";
 import { useControlContext } from "../Context/ControlContext";
+import { useLocalParticipant } from "@livekit/components-react";
 export const useRole = () => {
+    const room = useRoomContext();
+   const {localParticipant} = useLocalParticipant();
    //  const room = useRoomContext();
    //  const {setMic , setCameraView , setMute} = useControlContext();
-    const MuteParticipant = async (identity:any , isCurrentlyMicEnabled:any ) => {
+    const DisabledMicParticipant = async (identity:any , isCurrentlyMicEnabled:any ) => {
        try{
        const roomName = localStorage.getItem("sessionName")?.toString();
        ControlParticipant.identity = identity;
@@ -15,13 +18,31 @@ export const useRole = () => {
       //  setCameraView(false);
       //  setMic(false);
     //    setMute((prev) => !prev);
-       const res =await Role(ControlParticipant);
+       const res = await Role(ControlParticipant);
        console.log( res.data);
        }catch(err:any){
         console.log("errors" , err);
        }
        
     }
-    return {MuteParticipant}
+    
+  
+   const MuteParticipant = async (identity: string) => {
+        try {
+            const payload = JSON.stringify({ 
+                type: 'SOFT_MUTE', 
+                targetIdentity: identity 
+            });
+            const encoder = new TextEncoder();
+            
+            await room.localParticipant.publishData(encoder.encode(payload), { 
+                reliable: true, 
+                topic: "control" 
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return {DisabledMicParticipant , MuteParticipant}
 }
 
