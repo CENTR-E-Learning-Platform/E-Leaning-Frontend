ee829f37c1216d5e80f7db7f756d0c1b360f7965
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-03-26 22:58:24 +0200

    Adding Line Between Filter Elements Page

---
+++ src/Features/ExploreTeacher/Components/Explore/LineBetweenFilterElements.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LineBetweenFilterElements = () => {
+  return <>
+    <section className='Line'>
+        <div className="w-[250px] mb-[22px] border border-[#D1D5DB]"></div>
+    </section>
+  </>
+}
+
+export default LineBetweenFilterElements
