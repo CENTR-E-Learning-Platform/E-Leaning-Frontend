d6ea5e1804860732dacacd094e8ea9d5e861e567
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-02 10:50:13 +0200

    Adding ButtomReserveSession Page

---
+++ src/Features/ExploreTeacher/Components/Explore/ButtomReserveSession.tsx
@@ -0,0 +1,15 @@
+
+const ButtomReserveSession = () => {
+  return <>
+    <section className="w-[300px] h-[66px]">
+        <p className="text-[14px] font-bold text-[#E15254]">
+            2 seats left
+        </p>
+        <button className="font-semibold cursor-pointer w-[300px] h-[45px] flex justify-center items-center text-[18px] text-[#F9FBFC] bg-[#525FE1] rounded-[8px] p-4">
+            Reserve Session
+        </button>
+    </section>
+  </>
+}
+
+export default ButtomReserveSession
