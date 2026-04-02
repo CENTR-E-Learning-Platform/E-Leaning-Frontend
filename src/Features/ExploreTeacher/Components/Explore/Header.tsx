49b5ae986774f91ff836b64cc1f427b8b8492eac
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-02 10:49:55 +0200

    Adding Header Page

---
+++ src/Features/ExploreTeacher/Components/Explore/Header.tsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return <>
+        <section className="flex justify-between items-center mb-[30px]">
+          <h2 className="text-[#2A2D34] font-bold text-[32px]">
+            Explore teachers
+          </h2>
+          <p className="text-[#2A2D34] font-bold text-[18px]">
+            224 teachers available
+          </p>
+        </section>
+  </>
+}
+
+export default Header
