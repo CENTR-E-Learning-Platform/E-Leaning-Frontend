bb98129da4e34b9485e2f8aa2dd7e698687c2dc4
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-02 10:49:31 +0200

    Adding LayerBackgroundTeacher Page

---
+++ src/Features/ExploreTeacher/Components/Explore/LayerBackgroundTeacher.tsx
@@ -0,0 +1,14 @@
+
+const LayerBackgroundTeacher = () => {
+  return<>
+    <section className="layerBackGround top-[-23px] left-[-80px] -z-10 absolute ">
+        <img
+            className="w-[236px] rotate-[99.81deg] h-[239]"
+            src="../../../../../src/assets/images/BackTeacher.png"
+            alt="BackTeacher"
+        />
+    </section>
+  </>
+}
+
+export default LayerBackgroundTeacher
