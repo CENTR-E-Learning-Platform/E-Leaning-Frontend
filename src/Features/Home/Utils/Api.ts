cb87b7ec845b50870e981342996838294dc7259b
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-08 00:41:23 +0200

    Adding Api

---
+++ src/Features/Home/Utils/Api.ts
@@ -0,0 +1,6 @@
+const BASE_URL = "https://localhost:7251";
+const GET_STUDENT_DASHBOARD_INFO = `${BASE_URL}/api/StudentDashboard/GetStudentDashboardInfo`;
+const UPCOMING_CLASSES = `${BASE_URL}/api/StudentDashboard/upcoming-classes`;
+const RECOMMENDED_TEACHERS = `${BASE_URL}/api/StudentDashboard/recommended-teachers`;
+
+export {GET_STUDENT_DASHBOARD_INFO , UPCOMING_CLASSES , RECOMMENDED_TEACHERS}
