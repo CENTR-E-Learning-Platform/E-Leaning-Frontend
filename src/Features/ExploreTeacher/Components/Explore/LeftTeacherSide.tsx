c64422bc16b632cf67970caf729e1ce907ecd164
Abdelrhmancs <ght7234@gmail.com>
2026-04-07 22:03:57 +0200

    refactor LeftTeacherSide component for improved layout and styling consistency

--- src/Features/ExploreTeacher/Components/Explore/LeftTeacherSide.tsx
+++ src/Features/ExploreTeacher/Components/Explore/LeftTeacherSide.tsx
@@ -1,21 +1,21 @@
 import ButtomReserveSession from "./ButtomReserveSession"
 import SelectTime from "./SelectTime"
 
-
 function LeftTeacherSide() {
   return <>
-    <section className="LeftTeacher w-[300px] h-[266px]">
-        <div className="w-[300px] mb-7 flex justify-between items-start">
-            <div className="w-[131px] flex justify-between items-start gap-2">
+    <section className="LeftTeacher w-[285px] h-[239px]">
+        <div className="w-[285px] mb-6 flex justify-between items-start">
+            <div className="w-[124px] flex justify-between items-start gap-2">
                 <img
                 src="../../../../../src/assets/icons/MoneyIcon.svg"
                 alt="MoneyIcon"
+                className="w-[22px] h-[22px]"
                 />
                 <div>
-                    <p className="font-bold text-[#525FE1] text-[24px]">
+                    <p className="font-bold text-[#525FE1] text-[22px]">
                         Egp 100
                     </p>
-                    <p className="font-medium text-[14px] text-[#2A2D34]">
+                    <p className="font-medium text-[13px] text-[#2A2D34]">
                         per session
                     </p>
                 </div>
@@ -24,6 +24,7 @@ function LeftTeacherSide() {
                 <img
                 src="../../../../../src/assets/icons/heartIcon.svg"
                 alt="heartIcon"
+                className="w-[22px] h-[22px]"
                 />
             </div>
         </div>
