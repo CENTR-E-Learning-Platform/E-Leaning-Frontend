a60c9fc175efd181d4e22ae7c0f9b589e7136ee8
Abdelrhmancs <ght7234@gmail.com>
2026-04-08 10:54:09 +0200

    add ADD_WALLET and BALANCE API endpoints to Apis.ts

--- src/Features/Setting/Utils/Apis.ts
+++ src/Features/Setting/Utils/Apis.ts
@@ -4,13 +4,16 @@ const UPDATE_STUDENT_PROFILE_API = `${BASE_URL}/api/StudentProfile/Update_Studne
 const DELETE_FILE_API = `${BASE_URL}/api/TeacherProfile/Delete%20File`;
 const UPLOADIMAGE_API = `${BASE_URL}/api/TeacherProfile/Add%20Image`;
 const CHANGE_PASSWORD = `${BASE_URL}/Account/change-password`
-
+const ADD_WALLET = `${BASE_URL}/api/TeacherPayment/Add%20Wallet`
+const BALANCE = `${BASE_URL}/api/TeacherPayment/balance`
 
 export{
     GET_STUDENT_PROFILE_API,
     UPDATE_STUDENT_PROFILE_API,
     DELETE_FILE_API,
     UPLOADIMAGE_API,
-    CHANGE_PASSWORD
+    CHANGE_PASSWORD,
+    ADD_WALLET,
+    BALANCE
 }
 
