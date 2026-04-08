c351d3e1f7ffddc8438930c79a0dd76915ba1b14
Abdelrhmancs <ght7234@gmail.com>
2026-04-08 10:53:49 +0200

    refactor Attend component to use dynamic participant images and improve layout

--- src/Features/Streaming/Components/meeting/Attend.tsx
+++ src/Features/Streaming/Components/meeting/Attend.tsx
@@ -1,37 +1,30 @@
-import sene from "../../../../assets/images/sene.jpg";
-import zeng from "../../../../assets/images/zwng.jpg";
-import moz from "../../../../assets/images/moz.jpg";
 import attend from "../../../../assets/icons/attend.svg";
 import menu from "../../../../assets/icons/menu.svg";
 import { useParticipant } from "../../Hooks/useParticipant";
+import { BASE_URL } from "../../Utils/Apis";
+
 const Attend = ({ click }: any) => {
   const participants = useParticipant();
+
   return (
     <>
       {!click && (
-        <div className= " h-[48px] bg-[#393D44] rounded-[43px] flex items-center p-[4px] cursor-pointer">
+        <div className="h-[48px] bg-[#393D44] rounded-[43px] flex items-center p-[4px] cursor-pointer">
           <div className="flex justify-center">
-            <img
-              src={sene}
-              className="w-[40px] h-[39px] rounded-full -ml-2 first:ml-0 border-[1px] border-[#F9FBFC]"
-              alt=""
-            />
-            <img
-              src={moz}
-              className="w-[40px] h-[39px] rounded-full -ml-2 first:ml-2 border-[1px] border-[#F9FBFC]"
-              alt=""
-            />
-            <img
-              src={zeng}
-              className="w-[40px] h-[39px] rounded-full -ml-2 first:ml-2 border-[1px] border-[#F9FBFC]"
-              alt=""
-            />
+            {participants.tracks.slice(0, 3).map((track: any, index: number) => (
+              <img
+                key={index}
+                src={`${BASE_URL}/${track.participant.attributes["UserImage"]}`}
+                className="w-[40px] h-[39px] rounded-full -ml-2 first:ml-0 border-[1px] border-[#F9FBFC] object-cover"
+                alt=""
+              />
+            ))}
           </div>
-         {  participants.tracks.length  > 1 &&(
-           <h1 className="text-[16px] text-[#F9FBFC] ms-[8px]">
-            + {participants.tracks.length - 1}
-          </h1>
-         )}
+          {participants.tracks.length > 3 && (
+            <h1 className="text-[16px] text-[#F9FBFC] ms-[8px]">
+              + {participants.tracks.length - 3}
+            </h1>
+          )}
         </div>
       )}
       {click && (
@@ -39,7 +32,7 @@ const Attend = ({ click }: any) => {
           <div className="flex items-center">
             <img
               src={attend}
-              className="w-[19px] h-[16px]  me-[13px] ms-[10px] "
+              className="w-[19px] h-[16px] me-[13px] ms-[10px] "
               alt=""
             />
             <h1 className="text-[16px] text-[#F9FBFC] me-[13px]">
@@ -48,7 +41,7 @@ const Attend = ({ click }: any) => {
           </div>
           <img
             src={menu}
-            className="w-[4px] h-[17px]  me-[13px] ms-[10px] "
+            className="w-[4px] h-[17px] me-[13px] ms-[10px] "
             alt=""
           />
         </div>
@@ -56,4 +49,5 @@ const Attend = ({ click }: any) => {
     </>
   );
 };
+
 export default Attend;
