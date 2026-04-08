514ca610ead0295afb961e041319bedb71556708
Abdelrhmancs <ght7234@gmail.com>
2026-04-08 10:53:30 +0200

    refactor ChangePassword component for improved styling and layout consistency

--- src/Features/Setting/Components/Security/ChangePassword.tsx
+++ src/Features/Setting/Components/Security/ChangePassword.tsx
@@ -5,30 +5,30 @@ import password_icon from "../../../../assets/icons/material-symbols_lock-outlin
 import { useChangePassword } from "../../Hooks/useChangePassword";
 
 const ChangePassword = () => {
-  
-  const [openNewEye, setOpenNewEye] = useState([ false , false , false ]);
+  const [openNewEye, setOpenNewEye] = useState([false, false, false]);
   const formik = useChangePassword();
 
-  return <>
-    <section className="ChangePassword-section">
-        <div className="w-[600px] text-[#2A2D34] border border-[#E8EAED] rounded-[8px] bg-[#FFFFFF] p-7.5">
-          <h2 className="text-[24px] font-semibold leading-[13px] mb-7.75">
+  return (
+    <>
+      <section className="ChangePassword-section">
+        <div className="w-[570px] text-[#2A2D34] border border-[#E8EAED] rounded-[8px] bg-[#FFFFFF] p-7">
+          <h2 className="text-[22px] font-semibold leading-[13px] mb-7">
             Change Password
           </h2>
 
           <form onSubmit={formik.handleSubmit}>
-
-            <div className="flex flex-col relative mt-[5px]">
+            <div className="flex flex-col relative mt-[4px]">
               <label
                 htmlFor="currentPassword"
-                className="text-[#2A2D34] text-[16px] font-normal"
+                className="text-[#2A2D34] text-[15px] font-normal"
               >
                 Current password
               </label>
               <input
                 id="currentPassword"
-                className={`w-[540px] h-[52px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
-                  formik.touched.currentPassword && formik.errors.currentPassword
+                className={`w-[513px] h-[46px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
+                  formik.touched.currentPassword &&
+                  formik.errors.currentPassword
                     ? "border-red-600"
                     : formik.touched.currentPassword &&
                       formik.errors.currentPassword
@@ -43,36 +43,38 @@ const ChangePassword = () => {
               />
               <img
                 src={password_icon}
-                className="absolute left-2.5 top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-2.5 top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Password icon"
               />
               <img
                 src={openNewEye[0] ? eye_icon : skip_eye_icon}
-                className="absolute left-[500px] cursor-pointer top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-[475px] cursor-pointer top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Toggle visibility"
-                onClick={() => setOpenNewEye([!openNewEye[0], openNewEye[1], openNewEye[2]])}
+                onClick={() =>
+                  setOpenNewEye([!openNewEye[0], openNewEye[1], openNewEye[2]])
+                }
               />
-              {formik.touched.currentPassword && formik.errors.currentPassword && (
-                <p className="text-red-600 text-[12px]">
-                  {formik.errors.currentPassword}
-                </p>
-              )}
+              {formik.touched.currentPassword &&
+                formik.errors.currentPassword && (
+                  <p className="text-red-600 text-[11px]">
+                    {formik.errors.currentPassword}
+                  </p>
+                )}
             </div>
 
-            <div className="flex flex-col relative mt-[5px]">
+            <div className="flex flex-col relative mt-[4px]">
               <label
                 htmlFor="newPassword"
-                className="text-[#2A2D34] text-[16px] font-normal"
+                className="text-[#2A2D34] text-[15px] font-normal"
               >
                 New password
               </label>
               <input
                 id="newPassword"
-                className={`w-[540px] h-[52px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
+                className={`w-[513px] h-[46px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
                   formik.touched.newPassword && formik.errors.newPassword
                     ? "border-red-600"
-                    : formik.touched.newPassword &&
-                      formik.errors.newPassword
+                    : formik.touched.newPassword && formik.errors.newPassword
                     ? "border-[#525FE1]"
                     : "border-[#6D7588]"
                 }`}
@@ -84,33 +86,36 @@ const ChangePassword = () => {
               />
               <img
                 src={password_icon}
-                className="absolute left-2.5 top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-2.5 top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Password icon"
               />
               <img
                 src={openNewEye[1] ? eye_icon : skip_eye_icon}
-                className="absolute left-[500px] cursor-pointer top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-[475px] cursor-pointer top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Toggle visibility"
-                onClick={() => setOpenNewEye([openNewEye[0], !openNewEye[1], openNewEye[2]])}
+                onClick={() =>
+                  setOpenNewEye([openNewEye[0], !openNewEye[1], openNewEye[2]])
+                }
               />
               {formik.touched.newPassword && formik.errors.newPassword && (
-                <p className="text-red-600 text-[12px]">
+                <p className="text-red-600 text-[11px]">
                   {formik.errors.newPassword}
                 </p>
               )}
             </div>
 
-            <div className="flex flex-col relative mt-[5px]">
+            <div className="flex flex-col relative mt-[4px]">
               <label
                 htmlFor="confirmPassword"
-                className="text-[#2A2D34] text-[16px] font-normal"
+                className="text-[#2A2D34] text-[15px] font-normal"
               >
                 Confirm password
               </label>
               <input
                 id="confirmPassword"
-                className={`w-[540px] h-[52px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
-                  formik.touched.confirmPassword && formik.errors.confirmPassword
+                className={`w-[513px] h-[46px] rounded-[8px] pl-[35px] pr-10 border-2 border-[#D1D5DB] focus:outline-none ${
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
                     ? "border-red-600"
                     : formik.touched.confirmPassword &&
                       formik.errors.confirmPassword
@@ -125,33 +130,36 @@ const ChangePassword = () => {
               />
               <img
                 src={password_icon}
-                className="absolute left-2.5 top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-2.5 top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Password icon"
               />
               <img
                 src={openNewEye[2] ? eye_icon : skip_eye_icon}
-                className="absolute left-[500px] cursor-pointer top-[50px] -translate-y-1/2 w-[24px]"
+                className="absolute left-[475px] cursor-pointer top-[45px] -translate-y-1/2 w-[22px]"
                 alt="Toggle visibility"
-                onClick={() => setOpenNewEye([openNewEye[0], openNewEye[1], !openNewEye[2]])}
+                onClick={() =>
+                  setOpenNewEye([openNewEye[0], openNewEye[1], !openNewEye[2]])
+                }
               />
-              {formik.touched.confirmPassword && formik.errors.confirmPassword && (
-                <p className="text-red-600 text-[12px]">
-                  {formik.errors.confirmPassword}
-                </p>
-              )}
+              {formik.touched.confirmPassword &&
+                formik.errors.confirmPassword && (
+                  <p className="text-red-600 text-[11px]">
+                    {formik.errors.confirmPassword}
+                  </p>
+                )}
             </div>
 
             <button
-              className="font-bold text-[#F9FBFC] leading-[13px] cursor-pointer text-[16px] w-[540px] h-[52px] bg-[#525FE1] rounded-[8px] mt-[16px]"
+              className="font-bold text-[#F9FBFC] leading-[13px] cursor-pointer text-[15px] w-[513px] h-[46px] bg-[#525FE1] rounded-[8px] mt-[14px]"
               type="submit"
             >
               Update Password
             </button>
           </form>
-
         </div>
-    </section>
-  </>
-}
+      </section>
+    </>
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
