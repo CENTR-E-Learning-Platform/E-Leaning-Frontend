5e71e9549eb137edb82cb6b8730ef20d3bcda584
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-08 22:59:29 +0200

    Add GET_TEACHER_DASHBOARD_INFO API endpoint to Api.ts

--- src/Features/Home/Utils/Api.ts
+++ src/Features/Home/Utils/Api.ts
@@ -2,5 +2,8 @@ const BASE_URL = "https://localhost:7251";
 const GET_STUDENT_DASHBOARD_INFO = `${BASE_URL}/api/StudentDashboard/GetStudentDashboardInfo`;
 const UPCOMING_CLASSES = `${BASE_URL}/api/StudentDashboard/upcoming-classes`;
 const RECOMMENDED_TEACHERS = `${BASE_URL}/api/StudentDashboard/recommended-teachers`;
+const GET_TEACHER_DASHBOARD_INFO = `${BASE_URL}/api/TeacherDashboard/GetTeacherDashboardInfo`;
 
-export {GET_STUDENT_DASHBOARD_INFO , UPCOMING_CLASSES , RECOMMENDED_TEACHERS}
+
+
+export {GET_STUDENT_DASHBOARD_INFO , UPCOMING_CLASSES , RECOMMENDED_TEACHERS, GET_TEACHER_DASHBOARD_INFO}
