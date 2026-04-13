34b05bae3a36f57d96aeaa3eb83a24bc055eb601
Abdelrhmancs <ght7234@gmail.com>
2026-04-14 00:46:03 +0200

    Add QuizSummaryBar component for displaying quiz summary information

---
+++ src/Features/Quiz/Components/QuizSummaryBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import date from '../../../assets/icons/date.svg';
+import book from '../../../assets/icons/book.svg';
+import chem from '../../../assets/icons/chems.svg';
+const QuizSummaryBar: React.FC = () => {
+  return (
+    <div className="flex flex-row items-center px-5 py-4 gap-[14px] w-[1045px] h-[58px] bg-[#F1F4F9] border-l-4 border-l-[#525FE1] shadow-sm rounded-lg font-[Poppins]">
+      <div className="flex flex-row items-center gap-2">
+     <img src={book} className='w-[14px] h-[18px]' alt="" />
+        <span className="font-semibold text-base text-[#181C20] text-[14px] leading-6">
+          Stoichiometry Quiz
+        </span>
+      </div>
+
+      <div className="w-[1px] h-4 bg-[#E2E8F0]" />
+
+      <div className="flex flex-row items-center gap-2">
+        <img src={chem} className='w-[16px] h-[18px]' alt="" />
+        <span className="font-normal text-base text-[#475569] text-[14px] leading-6">
+          Chemistry - Prep 2
+        </span>
+      </div>
+
+      <div className="w-[1px] h-4 bg-[#E2E8F0]" />
+      <img src={date} className='w-[16px]' alt="" />
+      <div className="flex flex-row items-center gap-1">
+        <span className="font-normal text-base text-[#475569] text-[14px] leading-6">
+          Due: Apr 10, 2026
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default QuizSummaryBar;
