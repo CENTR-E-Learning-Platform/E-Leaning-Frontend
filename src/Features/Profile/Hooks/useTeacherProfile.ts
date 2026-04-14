6f9c3f778edb6fff5618c540a32fbeac6b4e31fe
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-14 21:28:36 +0200

    Update UseTeacherProfile Hook

--- src/Features/Profile/Hooks/useTeacherProfile.ts
+++ src/Features/Profile/Hooks/useTeacherProfile.ts
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { GetTeacherProfileData } from "../Services/TeacherProfileAPI";
 
-export const useTeacherProfile = () => {
+export const useTeacherProfile = (isProfilePage: boolean) => {
+  const { data, refetch } = useQuery({
+    queryKey: ["teacherProfile"],
+    queryFn: GetTeacherProfileData,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    staleTime: Infinity,
+    enabled: isProfilePage,
+  });
 
-   const { data  , refetch} = useQuery({
-        queryKey: ["teacherProfile"],
-        queryFn: GetTeacherProfileData,
-        refetchOnWindowFocus: false,
-    });
-
-    return {data , refetch};
-}
+  return { data, refetch };
+};
