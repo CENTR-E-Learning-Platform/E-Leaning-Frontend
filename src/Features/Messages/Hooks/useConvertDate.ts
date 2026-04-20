2ea6750a5119c80157f06ffdef0270252039c8ac
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-21 00:20:12 +0200

    Adding Convert Date Hook

---
+++ src/Features/Messages/Hooks/useConvertDate.ts
@@ -0,0 +1,34 @@
+export const useConverDate = () => {
+  const formatTime = (dateString: string) => {
+    const date = new Date(dateString);
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+    const messageDay = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+    );
+
+    const diffTime = today.getTime() - messageDay.getTime();
+    const diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+    if (diffDays === 0) {
+      return date
+        .toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })
+        .toLowerCase();
+    }
+
+    if (diffDays === 1) {
+      return "yesterday";
+    }
+
+    return date.toLocaleDateString("en-GB");
+  };
+
+  return formatTime;
+};
