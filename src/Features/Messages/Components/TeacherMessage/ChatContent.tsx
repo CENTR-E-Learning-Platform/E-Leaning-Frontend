0a180d7fccdd7176109883996709f14fa344408d
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-22 23:42:04 +0200

    Update ChatContent Component

--- src/Features/Messages/Components/TeacherMessage/ChatContent.tsx
+++ src/Features/Messages/Components/TeacherMessage/ChatContent.tsx
@@ -1,3 +1,6 @@
+import { useGetChatConversation } from "../../Hooks/useGetChatConversation"
+import useSignalR from "../../Hooks/useSignalR"
+import { BASE_URL } from "../../Utils/Api"
 import ChatHeader from "./ChatHeader"
 import ChatInput from "./ChatInput"
 import DateDivider from "./DateDivider"
@@ -7,18 +10,22 @@ import { TeacherFileMessage } from "./TeacherFileMessage"
 import { TeacherTextMessage } from "./TeacherTextMessage"
 
 const ChatContent = () => {
+  const token = `${localStorage.getItem("token")}`;
+  const { refetch } = useGetChatConversation();
+  const signalR = useSignalR(token, BASE_URL, refetch);
+
   return <>
-    <section className="ChatContent relative bg-[#F3F6FF] w-[800px] h-[calc(100vh-66px)]">
+    <section className="ChatContent border border-[#C4C5D94D] relative bg-[#F3F6FF] w-[800px] h-[calc(100vh-66px)]">
        <ChatHeader/>
         <div className="p-[32px]">
             <DateDivider/>
             <div className="mb-6"></div>
             <StudentMessage/>
             <div className="mb-6"></div>
-            <TeacherTextMessage/>
+            <TeacherTextMessage messages={signalR.messages}/>
             <div className="mb-6"></div>
             <TeacherFileMessage/>
-            <ChatInput/>
+            <ChatInput connection={signalR.connection}/>
         </div>
     </section>
   </>
