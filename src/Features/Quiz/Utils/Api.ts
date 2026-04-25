28a268d021d72b2a5c5058ec7068d93107fbc6a8
Abdelrhmancs <ght7234@gmail.com>
2026-04-25 03:59:29 +0300

    add API endpoints for quiz basic info, status, and student submissions

--- src/Features/Quiz/Utils/Api.ts
+++ src/Features/Quiz/Utils/Api.ts
@@ -3,9 +3,15 @@ const ADD_QUIZ_API = `${BASE_URL}/api/TeacherQuiz/add-quiz`;
 const GET_ALL_CLASSES_QUIZ_API = `${BASE_URL}/api/TeacherQuiz/get-sessions-data`;
 const ATTEND_QUIZ_API = `${BASE_URL}/api/StudentQuiz/attend-quiz`;
 const SUBMIT_QUIZ_API = `${BASE_URL}/api/StudentQuiz/submit-quiz`;
+const QUIZ_BASIC_INFO_API  = `${BASE_URL}/api/TeacherQuiz/get-quiz-basic-info`;
+const QUIZ_STATUS_API = `${BASE_URL}/api/TeacherQuiz/get-quiz-submission-status-Performance-Preview`;
+const QUIZ_SEARCH_API = `${BASE_URL}/api/TeacherQuiz/get-student-submissions`;
 export{
     ADD_QUIZ_API,
     GET_ALL_CLASSES_QUIZ_API,
     ATTEND_QUIZ_API,
-    SUBMIT_QUIZ_API
+    SUBMIT_QUIZ_API,
+    QUIZ_BASIC_INFO_API,
+    QUIZ_STATUS_API,
+    QUIZ_SEARCH_API
 }
