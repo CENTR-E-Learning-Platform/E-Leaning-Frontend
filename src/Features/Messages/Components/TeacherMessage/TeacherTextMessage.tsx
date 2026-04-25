3a6fdc3eb113183057e1ea1d22b67d2e2cb05d9a
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-04-25 18:14:46 +0300

    Update TeacherTextMessage Component

--- src/Features/Messages/Components/TeacherMessage/TeacherTextMessage.tsx
+++ src/Features/Messages/Components/TeacherMessage/TeacherTextMessage.tsx
@@ -1,37 +1,3 @@
-// import { useState } from "react";
-// import useSignalR from "../../Hooks/useSignalR";
-// import { BASE_URL } from "../../Utils/Api";
-
-// export const TeacherTextMessage  = () => {
-//   const [message, setMessage] = useState("");
-//   const token = `${localStorage.getItem("token")}`;
-//   const { connection, messages, typingUser } = useSignalR(token, BASE_URL);
-//   return (
-//     <div className="flex flex-col justify-end items-end p-0 w-[730px] self-stretch">
-//       <div className="flex flex-row items-start p-0 gap-[15.2px] w-[391.4px] max-w-[440.04px]">
-//         <div className="flex flex-col items-end p-0 w-[345.8px]">
-//           <div className="relative flex flex-col items-start pt-[14.4px] pr-[20.5px] pb-[14.4px] pl-[15.2px] w-[345.8px] bg-[#525FE1] rounded-l-[16px] rounded-br-[16px] shadow-sm overflow-hidden">
-//             <div className="absolute inset-0 bg-white opacity-[0.002] shadow-[0px_4px_6px_-1px_rgba(0,0,0,0.1)]" />
-//             <p className="z-10 flex items-center w-[308.96px] font-['Poppins'] font-normal text-[15px] leading-[21.6px] text-white">
-//               Of course Ali! I checked it already — you did well on most of the problems. Just a small mistake in question 4. Let me send you a correction note.
-//             </p>
-//           </div>
-//           <div className="flex flex-col items-start pt-[7.2px] px-[3.8px] pb-0 w-[41.85px] h-[20.7px]">
-//             <span className="w-[38px] h-[13.5px] font-['Poppins'] font-light text-[10px] leading-[13.5px] text-[#434656]">
-//               8:17 am
-//             </span>
-//           </div>
-//         </div>
-//         <div className="flex flex-row justify-center items-center w-[30.4px] h-[28.8px] bg-[#525FE1] rounded-full">
-//           <span className="w-[5.62px] h-[13.5px] font-['Poppins'] font-bold text-[10px] text-center text-white">
-//             T
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 type Message = {
   senderId: string;
   senderName: string;
@@ -71,32 +37,6 @@ export const TeacherTextMessage = ({ messages }: Props) => {
               </span>
             </div>
           </div>
-
-          {/* <div className="flex flex-row items-start p-0 gap-[15.2px] w-[391.4px] max-w-[440.04px]">
-            <div className="flex flex-col items-end p-0 w-[345.8px]">
-              
-              <div className="relative flex flex-col items-start pt-[14.4px] pr-[20.5px] pb-[14.4px] pl-[15.2px] w-[345.8px] bg-[#525FE1] rounded-l-[16px] rounded-br-[16px] shadow-sm overflow-hidden">
-                <div className="absolute inset-0 bg-white opacity-[0.002] shadow-[0px_4px_6px_-1px_rgba(0,0,0,0.1)]" />
-                
-                <p className="z-10 flex items-center font-['Poppins'] font-normal text-[15px] leading-[21.6px] text-white">
-                  {msg.content}
-                </p>
-              </div>
-
-              <div className="flex flex-col items-start pt-[7.2px] px-[3.8px] pb-0 w-[41.85px] h-[20.7px]">
-                <span className="w-[38px] h-[13.5px] font-['Poppins'] font-light text-[10px] leading-[13.5px] text-[#434656]">
-                  8:17 am
-                </span>
-              </div>
-
-            </div>
-
-            <div className="flex flex-row justify-center items-center w-[30.4px] h-[28.8px] bg-[#525FE1] rounded-full">
-              <span className="w-[5.62px] h-[13.5px] font-['Poppins'] font-bold text-[10px] text-center text-white">
-                {msg.senderName}
-              </span>
-            </div>
-          </div> */}
         </div>
       ))}
     </div>
