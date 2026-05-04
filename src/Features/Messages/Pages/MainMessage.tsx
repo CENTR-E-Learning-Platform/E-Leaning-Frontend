5d36065280664dfc940f36e57400d57a5de37aa9
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-05-05 02:21:02 +0300

    Add MainMessage component to handle message display based on contact items state

---
+++ src/Features/Messages/Pages/MainMessage.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import ChatContent from "../Components/Message/ChatContent";
+import ContactList from "../Components/Message/ContactItem";
+import { ShareDataContactItems } from "../Contexts/ShareDataContactItems";
+import MessagesHeader from "../Components/Message/MessagesHeader";
+import EmptyTeacher from "../Components/EmptyMessage/EmptyTeacher";
+import { roleToAuth } from "../../../Utils/Constant";
+import EmptyStudent from "../Components/EmptyMessage/EmptyStudent";
+const MainMessage = () => {
+  
+  const { dataContactItem } = useContext(ShareDataContactItems);
+  const isTeacher = roleToAuth?.includes("Teacher") ? true : false;
+
+  return (
+    <>
+      <section className="MainMessage">
+        {dataContactItem.length === 0 ? (
+          <div className="controlMessage flex justify-start items-start bg-[#F9FBFC]">
+            <div className="left-content bg-[#F9FBFC] h-[calc(100vh-66px)] flex flex-col">
+              <MessagesHeader />
+              <ContactList />
+            </div>
+
+            <div className="center-content">
+              <ChatContent />
+            </div>
+
+            <div className="right-content"></div>
+          </div>
+        ) : isTeacher ? (
+          <EmptyTeacher />
+        ) : (
+          <EmptyStudent />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default MainMessage;
