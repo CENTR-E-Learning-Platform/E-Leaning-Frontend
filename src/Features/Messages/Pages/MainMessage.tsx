3c5699761c3cbb4a72b3fbef209c2e1283cd2617
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-05-05 17:30:47 +0300

    Refactor MainMessage component to improve data handling and conditional rendering

--- src/Features/Messages/Pages/MainMessage.tsx
+++ src/Features/Messages/Pages/MainMessage.tsx
@@ -4,26 +4,27 @@ import ContactList from "../Components/Message/ContactItem";
 import { ShareDataContactItems } from "../Contexts/ShareDataContactItems";
 import MessagesHeader from "../Components/Message/MessagesHeader";
 import EmptyTeacher from "../Components/EmptyMessage/EmptyTeacher";
-import { roleToAuth } from "../../../Utils/Constant";
 import EmptyStudent from "../Components/EmptyMessage/EmptyStudent";
 const MainMessage = () => {
-  
-  const { dataContactItem } = useContext(ShareDataContactItems);
-  const isTeacher = roleToAuth?.includes("Teacher") ? true : false;
+  const { dataContactItem , isTeacher } = useContext(ShareDataContactItems);
 
   return (
     <>
       <section className="MainMessage">
-        {dataContactItem.length === 0 ? (
+        {dataContactItem?.length === 0 ? (
           <div className="controlMessage flex justify-start items-start bg-[#F9FBFC]">
             <div className="left-content bg-[#F9FBFC] h-[calc(100vh-66px)] flex flex-col">
               <MessagesHeader />
               <ContactList />
             </div>
 
-            <div className="center-content">
-              <ChatContent />
-            </div>
+            {dataContactItem?.length >= 1 ? (
+              <div className="center-content">
+                <ChatContent />
+              </div>
+            ) : (
+              ""
+            )}
 
             <div className="right-content"></div>
           </div>
