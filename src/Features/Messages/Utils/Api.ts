f5524f2a94de51a1a12bb8cc6731ef8007202c52
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-05-05 17:30:54 +0300

    Update API constants to include CHAT_GET_MY_GROUPS and CHAT_GROUP_MESSAGES

--- src/Features/Messages/Utils/Api.ts
+++ src/Features/Messages/Utils/Api.ts
@@ -1,9 +1,12 @@
 const BASE_URL = "https://localhost:7251";
-const CHAT_GET_MESSAGES= `${BASE_URL}/Chat/get/messages`
-const CHAT_GET_CONVERSATION= `${BASE_URL}/Chat/get/conversations`
-
+const CHAT_GET_MESSAGES= `${BASE_URL}/Chat/get/messages`;
+const CHAT_GET_CONVERSATION= `${BASE_URL}/Chat/get/conversations`;
+const CHAT_GET_MY_GROUPS= `${BASE_URL}/Chat/my-groups`;
+const CHAT_GROUP_MESSAGES= `${BASE_URL}/Chat/group-messages`;
 export {
   CHAT_GET_MESSAGES,
   CHAT_GET_CONVERSATION,
-  BASE_URL
+  BASE_URL,
+  CHAT_GET_MY_GROUPS,
+  CHAT_GROUP_MESSAGES
 };
