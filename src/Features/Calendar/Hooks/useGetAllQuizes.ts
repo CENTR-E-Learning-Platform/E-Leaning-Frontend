2047b23bb4faf12f5d70be223d06562485712f3e
Abdelrhmancs <ght7234@gmail.com>
2026-05-07 02:47:01 +0300

    Add useGetAllQuizes hook to fetch and manage quiz data for teachers and students

---
+++ src/Features/Calendar/Hooks/useGetAllQuizes.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllQuizTeacher } from "../Services/getAllQuizTeacher";
+import { roleToAuth } from "../../../Utils/Constant";
+import { getAllQuizzesStudent } from "../Services/getAllQuizStudent";
+export const useGetAllQuizes = () => {
+    const isTeacher = !!roleToAuth?.includes("Teacher");
+     const { data , refetch } = useQuery({
+    queryKey: [isTeacher ? "getAllQuizTeacher" : "getAllQuizzesStudent"],
+    queryFn: async() =>{
+        if(isTeacher){
+            return (await getAllQuizTeacher());
+        }
+        return (await getAllQuizzesStudent());
+        
+    },
+    
+    staleTime: Infinity,
+    gcTime: Infinity,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+});
+    return {data , refetch}
+}
