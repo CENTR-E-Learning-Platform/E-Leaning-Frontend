d6390234908d1dcca4781568d4d06fc91b195394
Abdelrhmancs <ght7234@gmail.com>
2026-05-07 02:47:46 +0300

    Add quiz API endpoints for teachers and students in api.ts

--- src/Features/Calendar/Utils/api.ts
+++ src/Features/Calendar/Utils/api.ts
@@ -1,4 +1,6 @@
 const TEACHER_CLASSES = "api/Calendar/GetTeacherClasses";
 const STUDENT_CLASSES = "api/Calendar/GetStudentClasses";
 const BASE_URL = "https://localhost:7251";
-export {TEACHER_CLASSES , STUDENT_CLASSES , BASE_URL}
+const TEACHER_QUIZ = `${BASE_URL}/api/Calendar/get-teacher-quizzes-calendar`;
+const STUDENT_QUIZ = `${BASE_URL}/api/Calendar/get-student-quizzes-calendar`;
+export {TEACHER_CLASSES , STUDENT_CLASSES , BASE_URL , TEACHER_QUIZ , STUDENT_QUIZ};
