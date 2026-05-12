b4729db4e8ed61bddd1c701d460d11bb98876c91
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-05-12 19:46:21 +0300

    refactor: comment out conditional rendering for empty chat state in MainMessage component

--- src/Features/Messages/Pages/MainMessage.tsx
+++ src/Features/Messages/Pages/MainMessage.tsx
@@ -14,29 +14,29 @@ const MainMessage = () => {
   return (
     <>
       <section className="MainMessage">
-        {dataGetChatConversation?.data?.length === 0 ? (
+        {/* {dataGetChatConversation?.data?.length === 0 ? (
           isTeacher ? (
             <EmptyTeacher />
           ) : (
             <EmptyStudent />
           )
         ) : (
+        )} */}
         <div className="controlMessage flex justify-start items-start bg-[#F9FBFC]">
           <div className="left-content bg-[#F9FBFC] h-[calc(100vh-66px)] flex flex-col">
             <MessagesHeader />
             <ContactList />
           </div>
 
-          {dataGetChatConversation?.data?.length >= 1 ? (
             <div className="center-content">
               <ChatContent />
             </div>
+          {/* {dataGetChatConversation?.data?.length >= 1 ? (
           ) : (
             ""
-          )}
+          )} */}
           <div className="right-content"></div>
         </div>
-        )}
       </section>
     </>
   );
