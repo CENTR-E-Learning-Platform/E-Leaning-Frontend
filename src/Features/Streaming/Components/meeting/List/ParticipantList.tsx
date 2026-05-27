d8dd0d1ac1507ee8e37baf4f4aff6dfad27b8a0b
Abdelrhmancs <ght7234@gmail.com>
2026-05-27 03:56:31 +0300

    feat: implement streaming feature with room management, participant controls, and media handling

--- src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
+++ src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
@@ -3,17 +3,17 @@ import mic from '../../../../../assets/icons/mic2.svg';
 import disMic from '../../../../../assets/icons/disableMic.svg';
 import disrais from '../../../../../assets/icons/disablehand.svg';
 import kick from '../../../../../assets/icons/kick.svg';
-const participantList = ({onDisabled ,onMute }:any) => {
+const participantList = ({ onDisabled, onMute }: any) => {
   return (
     <>
-        <ParticipantComp 
-        func = {onMute}
-        rad = "rounded-t-[8px]" icon ={mic} color = "#F9FBFC" parg ="Mute Participant" />     
-        <ParticipantComp 
-        func = {onDisabled}
-        rad = "" icon ={disMic}  color = "#F9FBFC" parg ="Disable Mic" />     
-        <ParticipantComp rad = "" icon ={disrais} color = "#F9FBFC" parg ="Lower Hand" />     
-        <ParticipantComp rad = "rounded-b-[8px]" icon ={kick} color = "#D24747" parg ="Remove Participant" />     
+      <ParticipantComp
+        func={onMute}
+        rad="rounded-t-[8px]" icon={mic} color="#F9FBFC" parg="Mute Participant" />
+      <ParticipantComp
+        func={onDisabled}
+        rad="" icon={disMic} color="#F9FBFC" parg="Disable Mic" />
+      <ParticipantComp rad="" icon={disrais} color="#F9FBFC" parg="Lower Hand" />
+      <ParticipantComp rad="rounded-b-[8px]" icon={kick} color="#D24747" parg="Remove Participant" />
     </>
   )
 }
