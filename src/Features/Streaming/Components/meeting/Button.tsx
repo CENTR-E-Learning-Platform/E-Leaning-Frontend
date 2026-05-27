230cc09c01a95117565d5ed5f4e3092f09fa8675
Abdelrhmancs <ght7234@gmail.com>
2026-05-27 03:55:39 +0300

    feat: implement video streaming features including participant grid, reaction system, and room management utilities

--- src/Features/Streaming/Components/meeting/Button.tsx
+++ src/Features/Streaming/Components/meeting/Button.tsx
@@ -1,9 +1,9 @@
-const Button = (props:any) => {
-    return(
+const Button = (props: any) => {
+    return (
         <>
-            <button 
-            onClick={ props.func}
-            className={`bg-[#2A2D34] ${props.customStyle}  w-[48px] h-[48px] rounded-[8px] flex justify-center items-center cursor-pointer me-[4px] border-[2px] border-[#393D44] hover:bg-[#454950] transition duration-300`}>
+            <button
+                onClick={props.func}
+                className={`bg-[#2A2D34] ${props.customStyle}  w-[48px] h-[48px] rounded-[8px] flex justify-center items-center cursor-pointer me-[4px] border-[2px] border-[#393D44] hover:bg-[#454950] transition duration-300`}>
                 <img src={props.icons} className={props.size} alt="" />
             </button>
         </>
