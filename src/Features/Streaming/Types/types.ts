17aadc71b6583ef6fff299fcab44d4ee26fc6b6a
Abdelrhmancs <ght7234@gmail.com>
2026-05-27 03:57:00 +0300

    feat: implement streaming feature routes, API endpoints, and initial form configurations

--- src/Features/Streaming/Types/types.ts
+++ src/Features/Streaming/Types/types.ts
@@ -1,19 +1,22 @@
 
 const initialValues = {
-    Price:20,
-    Title : "",
-    StartTime: "",
-    DurationMinutes:60,
-    weeksNumber: 1,
-    Grade: 0,
-    Reminder:"01:00:00",
-    Description:""
-  };
+  Price: 20,
+  Title: "",
+  StartTime: "",
+  DurationMinutes: 60,
+  weeksNumber: 1,
+  Grade: 0,
+  Reminder: "01:00:00",
+  Description: "",
+  Subject: 0,
+};
 
 const ControlParticipant = {
-  roomName:"",
-  identity:"",
-  mute:false
+  identity: "",
+  roomName: "",
+  allowMic: false,
+  allowCamera: false,
+  allowScreenShare: false
 }
 
-export {initialValues , ControlParticipant}
+export { initialValues, ControlParticipant }
