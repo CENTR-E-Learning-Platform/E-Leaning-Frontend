646d7e4edcd9dd1fba02e157f816419e7ce1341d
Abdelrhmancs <ght7234@gmail.com>
2026-05-27 03:56:25 +0300

    feat: implement streaming room join flow with hardware control and device selection support

--- src/Features/Streaming/Components/meeting/List/ParticipantComp.tsx
+++ src/Features/Streaming/Components/meeting/List/ParticipantComp.tsx
@@ -1,14 +1,14 @@
-const ParticipantComp = ({rad, icon , parg , color , func}:any) => {
+const ParticipantComp = ({ rad, icon, parg, color, func }: any) => {
   return (
     <>
-        <div 
+      <div
         onClick={func}
         className={`w-[198px] h-[48px] ${rad} bg-[#393D44] flex  justify-between items-center px-[10px] cursor-pointer border-[1px] border-[#454950] hover:bg-[#454950] transition duration-300`}>
-            <div className='flex justify-center items-center'>
-                <img src={icon} className='w-[16px] h-[16px]' alt="" />
-                <h1 className={`text-[${color}] ms-[8px] text-[14px] font-[500]`}>{parg}</h1>
-            </div>
+        <div className='flex justify-center items-center'>
+          <img src={icon} className='w-[16px] h-[16px]' alt="" />
+          <h1 className={`text-[${color}] ms-[8px] text-[14px] font-[500]`}>{parg}</h1>
         </div>
+      </div>
     </>
   )
 }
