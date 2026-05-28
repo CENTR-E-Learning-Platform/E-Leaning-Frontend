96d874fd1666d685f84d1ebd9323c78ba7cf1c42
Abdelrhmancs <ght7234@gmail.com>
2026-05-28 04:12:01 +0300

    feat: implement remote participant permission management and raise hand handling in streaming meeting interface

--- src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
+++ src/Features/Streaming/Components/meeting/List/ParticipantList.tsx
@@ -1,21 +1,69 @@
-import ParticipantComp from "./ParticipantComp"
-import mic from '../../../../../assets/icons/mic2.svg';
-import disMic from '../../../../../assets/icons/disableMic.svg';
+import ParticipantComp from "./ParticipantComp";
+import micOn from '../../../../../assets/icons/mic.svg';
+import micOff from '../../../../../assets/icons/mic2.svg';
 import disrais from '../../../../../assets/icons/disablehand.svg';
-import kick from '../../../../../assets/icons/kick.svg';
-const participantList = ({ onDisabled, onMute }: any) => {
+import videoOn from '../../../../../assets/icons/video.svg';
+import videoOff from '../../../../../assets/icons/camera-disabled.svg';
+import shareOn from '../../../../../assets/icons/share.svg';
+
+interface ParticipantListProps {
+  isMicOn: boolean;
+  isCamOn: boolean;
+  isShareOn: boolean;
+  isHandRaised: boolean;
+  onMuteToggle?: () => void;
+  onCameraToggle?: () => void;
+  onShareToggle?: () => void;
+  onLowerHand?: () => void;
+}
+
+const ParticipantList = ({
+  isMicOn,
+  isCamOn,
+  isShareOn,
+  isHandRaised,
+  onMuteToggle,
+  onCameraToggle,
+  onShareToggle,
+  onLowerHand,
+}: ParticipantListProps) => {
   return (
     <>
       <ParticipantComp
-        func={onMute}
-        rad="rounded-t-[8px]" icon={mic} color="#F9FBFC" parg="Mute Participant" />
-      <ParticipantComp
-        func={onDisabled}
-        rad="" icon={disMic} color="#F9FBFC" parg="Disable Mic" />
-      <ParticipantComp rad="" icon={disrais} color="#F9FBFC" parg="Lower Hand" />
-      <ParticipantComp rad="rounded-b-[8px]" icon={kick} color="#D24747" parg="Remove Participant" />
-    </>
-  )
-}
+        func={onMuteToggle}
+        rad="rounded-t-[8px]"
+        icon={isMicOn ? micOff : micOn}
+        color="#F9FBFC"
+        parg={isMicOn ? "Mute Participant" : "Unmute Participant"}
+      />
 
-export default participantList
+      <ParticipantComp
+        func={onCameraToggle}
+        rad=""
+        icon={isCamOn ? videoOff : videoOn}
+        color="#F9FBFC"
+        parg={isCamOn ? "Disable Camera" : "Enable Camera"}
+      />
+
+      <ParticipantComp
+        func={onShareToggle}
+        rad={isHandRaised ? "" : "rounded-b-[8px]"}
+        icon={shareOn}
+        color="#F9FBFC"
+        parg={isShareOn ? "Disable Screen Share" : "Enable Screen Share"}
+      />
+
+      {isHandRaised && (
+        <ParticipantComp
+          func={onLowerHand}
+          rad="rounded-b-[8px]"
+          icon={disrais}
+          color="#F9FBFC"
+          parg="Lower Hand"
+        />
+      )}
+    </>
+  );
+};
+
+export default ParticipantList;
