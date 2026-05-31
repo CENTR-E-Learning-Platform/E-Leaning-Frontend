0a90d1ba91c77982ac618940d8004813ff6deebe
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-01 01:51:29 +0300

    feat: rename upcoming classes constants for clarity and add teacher upcoming classes endpoint

--- src/Features/Home/Utils/Api.ts
+++ src/Features/Home/Utils/Api.ts
@@ -1,9 +1,10 @@
 const BASE_URL = "https://localhost:7251";
 const GET_STUDENT_DASHBOARD_INFO = `${BASE_URL}/api/StudentDashboard/GetStudentDashboardInfo`;
-const UPCOMING_CLASSES = `${BASE_URL}/api/StudentDashboard/upcoming-classes`;
+const UPCOMING_CLASSES_STUDENT = `${BASE_URL}/api/StudentDashboard/upcoming-classes`;
+const UPCOMING_CLASSES_TEACHER = `${BASE_URL}/api/TeacherDashboard/GetUpcomingClasses`;
 const RECOMMENDED_TEACHERS = `${BASE_URL}/api/StudentDashboard/recommended-teachers`;
 const GET_TEACHER_DASHBOARD_INFO = `${BASE_URL}/api/TeacherDashboard/GetTeacherDashboardInfo`;
 
 
 
-export {GET_STUDENT_DASHBOARD_INFO , UPCOMING_CLASSES , RECOMMENDED_TEACHERS, GET_TEACHER_DASHBOARD_INFO}
+export {GET_STUDENT_DASHBOARD_INFO , UPCOMING_CLASSES_STUDENT, UPCOMING_CLASSES_TEACHER, RECOMMENDED_TEACHERS, GET_TEACHER_DASHBOARD_INFO ,BASE_URL}
