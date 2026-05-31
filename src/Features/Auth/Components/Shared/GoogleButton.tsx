d31d66a61a237cc3d97a942fa49656ea40bcb587
Abdelrhmancs <ght7234@gmail.com>
2026-05-31 17:56:59 +0300

    feat: implement landing page, authentication flows, and student quiz interface modules

--- src/Features/Auth/Components/Shared/GoogleButton.tsx
+++ src/Features/Auth/Components/Shared/GoogleButton.tsx
@@ -1,13 +1,25 @@
 import google_icon from "../../../../assets/icons/material-icon-theme_google.png";
 import { useGoogle } from "../../Hooks/useGoogle";
 
-const GoogleButton = () => {
+interface GoogleButtonProps {
+  onBeforeNavigate?: () => void;
+}
+
+const GoogleButton = ({ onBeforeNavigate }: GoogleButtonProps) => {
   const loginWithGoogle = useGoogle();
+
+  const handleClick = () => {
+    if (onBeforeNavigate) {
+      onBeforeNavigate();
+    } else {
+      loginWithGoogle();
+    }
+  };
 
   return (
     <button
       type="button"
-      onClick={loginWithGoogle}
+      onClick={handleClick}
       className="cursor-pointer flex justify-center items-center mt-[8px] text-[#2A2D34] w-[379px] h-[36px] border-[#6D7588] border-[1px] rounded-[4px]"
     >
       <img className="w-[17px] me-[5px]" src={google_icon} alt="Google icon" />
@@ -19,3 +31,4 @@ const GoogleButton = () => {
 };
 
 export default GoogleButton;
+
