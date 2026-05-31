2bde0e07d8901ad4891b803aaae63f45c0f3d0e8
Abdelrhmancs <ght7234@gmail.com>
2026-05-31 17:55:35 +0300

    feat: implement authentication, landing page, and streaming components with registration flow and quiz functionality

--- src/Features/Auth/Components/Log in/LoginBar.tsx
+++ src/Features/Auth/Components/Log in/LoginBar.tsx
@@ -7,7 +7,7 @@ const Bar = ({title}:any) => {
             <h1 className="font-bold text-[28px] text-[#2A2D34] mb-[16px]">
               {title}!
             </h1>
-            <nav className="
+            {/* <nav className="
             flex bg-[#FFFFFF] border-[1px] border-[#6D7588] rounded-[4px] w-[379px] h-[37px] p-[2px]">
               <NavLink
                 className="w-[50%] flex justify-center items-center bg-[#DCDFF9] rounded-[4px] text-[12px] font-bold text-[#525FE1] cursor-pointer"
@@ -21,7 +21,7 @@ const Bar = ({title}:any) => {
               >
                 Sign up
               </NavLink>
-            </nav>
+            </nav> */}
           </header>
   );
 }
