bb38ac72d9dca7c0dc3ec807b60d994006eea5dd
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-05-31 04:29:39 +0300

    refactor: add teacher prop to ButtomReserveSession and display available seats dynamically

--- src/Features/ExploreTeacher/Components/Explore/ButtomReserveSession.tsx
+++ src/Features/ExploreTeacher/Components/Explore/ButtomReserveSession.tsx
@@ -1,8 +1,11 @@
-const ButtomReserveSession = () => {
+interface Teacher {
+  closesetSessionAvailableSeats: number; 
+}
+const ButtomReserveSession = ({ teacher }: { teacher: Teacher }) => {
   return <>
     <section className="w-[285px] h-[60px]">
         <p className="text-[13px] font-bold text-[#E15254]">
-            2 seats left
+            {teacher.closesetSessionAvailableSeats} seats left
         </p>
         <button className="font-semibold cursor-pointer w-[285px] h-[40px] flex justify-center items-center text-[16px] text-[#F9FBFC] bg-[#525FE1] rounded-[8px] p-[14px]">
             Reserve Session
