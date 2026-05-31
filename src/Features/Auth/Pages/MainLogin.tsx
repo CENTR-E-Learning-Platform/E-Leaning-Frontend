8c3e5820cf3a775063b12711baea91089ff9de8d
Abdelrhmancs <ght7234@gmail.com>
2026-05-31 17:57:17 +0300

    feat: implement landing page, authentication, quiz, and streaming components with updated routing.

--- src/Features/Auth/Pages/MainLogin.tsx
+++ src/Features/Auth/Pages/MainLogin.tsx
@@ -1,6 +1,7 @@
 import image from "../../../assets/images/Online learning-amico 1.png";
 import LoginBar from "../Components/Log in/LoginBar";
 import LoginForm from "../Components/Log in/LoginForm";
+import logo from "../../../assets/icons/logo.svg";
 
 const MainLogin = () => {
 
@@ -8,12 +9,15 @@ const MainLogin = () => {
     <main className="h-[100vh] flex w-full">
       <section className="w-[50%] flex justify-center bg-[#F9FBFC]">
         <article className="flex flex-col items-center justify-center">
+          <header className="flex justify-center items-center mb-[3px]">
+            <img src={logo} className="w-[150px] h-full" alt="" />
+          </header>
           <div>
-            <LoginBar title = "Welcome back" />
+            <LoginBar title="Welcome back" />
           </div>
 
           {/* Login Form */}
-          <LoginForm/>
+          <LoginForm />
         </article>
       </section>
 
