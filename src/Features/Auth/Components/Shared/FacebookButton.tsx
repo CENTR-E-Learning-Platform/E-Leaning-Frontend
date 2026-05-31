31abe96c98c877075933dcbb6cc59bc6fd003331
Abdelrhmancs <ght7234@gmail.com>
2026-05-31 17:56:52 +0300

    feat: implement landing page components, authentication flows, and quiz participation interface

--- src/Features/Auth/Components/Shared/FacebookButton.tsx
+++ src/Features/Auth/Components/Shared/FacebookButton.tsx
@@ -1,11 +1,24 @@
 import facebook_icon from "../../../../assets/icons/logos_facebook.png";
 import { useFacebook } from "../../Hooks/useFacebook";
 
-const FacebookButton = () => {
+interface FacebookButtonProps {
+  onBeforeNavigate?: () => void;
+}
+
+const FacebookButton = ({ onBeforeNavigate }: FacebookButtonProps) => {
   const facebook = useFacebook();
+
+  const handleClick = () => {
+    if (onBeforeNavigate) {
+      onBeforeNavigate();
+    } else {
+      facebook();
+    }
+  };
+
   return (
     <button
-      onClick={facebook}
+      onClick={handleClick}
       type="button"
       className="cursor-pointer flex justify-center items-center mt-[8px] text-[#2A2D34] w-[379px] h-[36px] border-[#6D7588] border-[1px] rounded-[4px]"
     >
