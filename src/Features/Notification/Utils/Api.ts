130fa0169921cb0aac91814bfe80f96945c52ae8
Abdelrhmancs <ght7234@gmail.com>
2026-06-02 04:59:44 +0300

    feat: implement quiz fullscreen enforcement and add student payment flow infrastructure

---
+++ src/Features/Notification/Utils/Api.ts
@@ -0,0 +1,9 @@
+import { BASE_URL } from "../../Home/Utils/Api"
+const LATEST_NOTIFICATIONS_API = `${BASE_URL}/Notification/latest`;
+const HISTORY_NOTIFICATIONS_API = `${BASE_URL}/Notification/history`;
+const COUNT_UNREAD_NOTIFICATIONS_API = `${BASE_URL}/Notification/unread-count`;
+export {
+    LATEST_NOTIFICATIONS_API,
+    HISTORY_NOTIFICATIONS_API,
+    COUNT_UNREAD_NOTIFICATIONS_API
+}
