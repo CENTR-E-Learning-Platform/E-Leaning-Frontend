b4cd752ef9bfd71607125b525cbb75010cec383f
Abdelrhmancs <ght7234@gmail.com>
2026-06-02 04:57:21 +0300

    feat: implement authentication flows, notification system, quiz taking interface, and payment processing features

--- src/Features/Auth/Hooks/useforgetpasswordAndSendemail.ts
+++ src/Features/Auth/Hooks/useforgetpasswordAndSendemail.ts
@@ -1,48 +1,48 @@
 import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
-import {  SendEmail } from "../Services/sendemailAPI";
+import { SendEmail } from "../Services/sendemailAPI";
 import { emailSchema } from "../Validation/emailSchema";
 import { PassAndRepassSchema } from "../Validation/Pass&repas";
 import { forgetpassword } from "../Services/forgetpasswordAPI";
 import { useRegContext } from "../Contexts/RegContext";
 
 export const useSendEmail = () => {
-    const navigate = useNavigate();
-    const ClickSendEmail = () => {
-      navigate("/login/SendEmail");
-    }
-    ////////////////////////////////////////////////////////////////////////////
-    const formik = useFormik({
-        initialValues: {
-          email: "",
-        },
-        validationSchema: emailSchema,
-        onSubmit: async (values, { setSubmitting }) => {
-          try {
-            setSubmitting(true);
-            const res = await SendEmail(values);
-            localStorage.setItem("emailSendEmail", values.email);
-            if (res.data.message === "Email Resnet Successfully" || res.status === 200) {
-              navigate("/login/SendEmail/otp");
-            }
-            console.log(" Email:", res.data);
-          } catch (error: any) {
-            const MSError = error.response?.data.errors[0] || error.message;
-            console.error("Email failed:", MSError);
-            alert(MSError);
-          } finally {
-            setSubmitting(false);
-          }
-        },
-      });
-    
-      
-  return {ClickSendEmail , formik};
+  const navigate = useNavigate();
+  const ClickSendEmail = () => {
+    navigate("/login/SendEmail");
+  }
+  ////////////////////////////////////////////////////////////////////////////
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: emailSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        setSubmitting(true);
+        const res = await SendEmail(values.email);
+        localStorage.setItem("emailSendEmail", values.email);
+        if (res.data.message === "Email Resnet Successfully" || res.status === 200) {
+          navigate("/login/SendEmail/otp");
+        }
+        console.log(" Email:", res.data);
+      } catch (error: any) {
+        const MSError = error.response?.data.errors[0] || error.message;
+        console.error("Email failed:", MSError);
+        alert(MSError);
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
+
+  return { ClickSendEmail, formik };
 }
 
 export const useResetpassword = () => {
   const navigate = useNavigate();
-  
+
   const { setUserData } = useRegContext();
   const Email = localStorage.getItem("emailSendEmail");
   const OTPforgetPassword = localStorage.getItem("otpForForgetPassword");
@@ -51,14 +51,16 @@ export const useResetpassword = () => {
       NewPassword: "",
       confirmPassword: "",
     },
-    
+
     validationSchema: PassAndRepassSchema,
     onSubmit: async (values, { setSubmitting }) => {
       const finalResetPassword = {
-        newPassword: values.NewPassword,
-        confirmPassword: values.confirmPassword,
         email: Email,
         otp: OTPforgetPassword,
+        newPassword: values.NewPassword,
+        confirmPassword: values.confirmPassword,
+
+
       };
       setUserData(prev => prev ? { ...prev, password: values.NewPassword } : prev);
       console.log(finalResetPassword)
@@ -66,7 +68,7 @@ export const useResetpassword = () => {
         setSubmitting(true);
         const res = await forgetpassword(finalResetPassword);
         console.log(" Resetpassword successful:", res.data);
-        alert("Succes Resetpassword" );
+        alert("Succes Resetpassword");
         navigate("/login");
       } catch (error: any) {
         const MSError = error.response?.data.errors[0] || error.message;
