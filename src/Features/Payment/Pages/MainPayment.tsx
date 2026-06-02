a04422f1cc8dc198c7898806c6f8c85ee2d56d51
Abdelrhmancs <ght7234@gmail.com>
2026-06-02 05:00:22 +0300

    feat: implement payment integration modules and student quiz attempt functionality

--- src/Features/Payment/Pages/MainPayment.tsx
+++ src/Features/Payment/Pages/MainPayment.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import StaticPayment from '../Components/Payment/StaticPayment'
 
 const MainPayment = () => {
   return <>
     <div className="px-[188px] py-[28px]">
-        <nav className="text[16px] w-[230px] font-medium leading-[13px] tracking-[0] text-[#5A6272] mb-3">
-          Explore Teacher &gt; <span className="text-[#2A2D34]">Payment</span>
-        </nav>
-        <div className="flex justify-between gap-[30px]">
-            <div className="LeftSide">
-                <StaticPayment/>
-            </div>
-            <div className='RightSide'>
-                <Outlet/>
-            </div>
-            
+      <nav className="text[16px] w-[230px] font-medium leading-[13px] tracking-[0] text-[#5A6272] mb-3">
+        <NavLink to={'/Home'} >Home</NavLink> &gt; <span className="text-[#2A2D34]">Payment</span>
+      </nav>
+      <div className="flex justify-center items-center gap-[30px] ">
+        <div className="LeftSide">
+          {/* <StaticPayment/> */}
         </div>
+        <div className=''>
+          <Outlet />
+        </div>
+
+      </div>
     </div>
   </>
 }
