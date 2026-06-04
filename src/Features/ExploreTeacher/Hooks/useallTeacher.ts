5e95e79f7d4721463944ca871c7d067956b0a6fb
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-04 08:26:43 +0300

    feat: implement useGetAllTeachers hook for fetching and managing teacher data

---
+++ src/Features/ExploreTeacher/Hooks/useallTeacher.ts
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { sendAllTeacherData } from "../Services/allTeacherAPI";
+import { useState } from "react";
+
+export const useGetAllTeachers = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const teachersPerPage = 3;
+
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ["allTeachers", currentPage, teachersPerPage],
+    queryFn: () =>
+      sendAllTeacherData({
+        pageIndex: Number(currentPage),
+        pageSize: Number(teachersPerPage),
+      }),
+    retry: false,
+    enabled: true,
+    refetchOnMount: false,
+    staleTime: 60000,
+    refetchOnWindowFocus: false,
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+    refetch,
+    currentPage,
+    setCurrentPage,
+    teachersPerPage,
+  };
+};
