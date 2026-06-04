104e808c5a65110532a4a94e4cee7091fe73da05
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-04 08:31:58 +0300

    feat: add ALL_TEACHER_API endpoint for fetching all teachers

--- src/Features/ExploreTeacher/Utils/api.ts
+++ src/Features/ExploreTeacher/Utils/api.ts
@@ -1,6 +1,5 @@
 const API_BASE_URL = "https://localhost:7251";
 const SEARCH_TEACHER_API = `${API_BASE_URL}/Search`;
 const FILTER_TEACHER_API = `${API_BASE_URL}/Filter`;
-
-
-export {SEARCH_TEACHER_API, FILTER_TEACHER_API};
+const ALL_TEACHER_API = `${API_BASE_URL}/GetAllTeachers`;
+export {SEARCH_TEACHER_API, FILTER_TEACHER_API, ALL_TEACHER_API};
