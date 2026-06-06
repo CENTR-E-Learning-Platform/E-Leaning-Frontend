6dc9312853840684479903028dd0b7ab9d67ed7b
Abdelrhmancs <ght7234@gmail.com>
2026-06-06 17:40:47 +0300

    feat: implement quiz publishing status handling with QuizStatusModal

--- src/Features/Quiz/Pages/CreateNewQuiz.tsx
+++ src/Features/Quiz/Pages/CreateNewQuiz.tsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import HeaderSection from '../Components/HeaderSection';
 import AddNewQuestionModal from '../Components/AddNewQuestionModal';
 import { Outlet } from 'react-router-dom';
 import { useQuiz } from '../Context/QuizContext';
 import PublishQuizModal from '../Components/PublishQuizModal';
 import { useCreateQuiz } from '../Hooks/useCreateQuiz';
+import QuizStatusModal from '../Components/QuizStatusModal';
+
 const CreateNewQuiz = () => {
   const { createNewQuiz } = useCreateQuiz();
   const { isModalOpen, setIsModalOpen, setIsPublishModel, isPublishModel } = useQuiz();
   const sessionId = localStorage.getItem("sessionId")?.toString();
+
+  const [status, setStatus] = useState<'success' | 'error' | null>(null);
+
+  const handlePublish = async () => {
+    try {
+      await createNewQuiz(sessionId as string);
+      setIsPublishModel(false);
+      setStatus('success');
+    } catch (error) {
+      setIsPublishModel(false);
+      setStatus('error');
+    }
+  };
+
   return (
     <>
       <div className="flex justify-center items-center flex-col gap-[24px] mt-[50px] mb-[50px]">
@@ -15,9 +32,11 @@ const CreateNewQuiz = () => {
         <Outlet />
       </div>
       
-      {isPublishModel && <PublishQuizModal onCancel={() => setIsPublishModel(false)} onPublish={()=> createNewQuiz(sessionId as string)} />}
+      {isPublishModel && <PublishQuizModal onCancel={() => setIsPublishModel(false)} onPublish={handlePublish} />}
       
       {isModalOpen && <AddNewQuestionModal onClose={() => setIsModalOpen(false)} />}
+
+      {status && <QuizStatusModal status={status} onClose={() => setStatus(null)} />}
     </>
   );
 };
