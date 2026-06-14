a8dadedbe8ddbccc37465ae9404a9ec155d2d207
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-14 07:33:50 +0300

    Update ViewTeacher Component Page

--- src/Features/Profile/Pages/ViewTeacher.tsx
+++ src/Features/Profile/Pages/ViewTeacher.tsx
@@ -1,11 +1,16 @@
+import AboutTeacher from "../Components/ViewStudent/AboutTeacher"
 import AboutMe from "../Components/ViewTeacher/AboutMe"
 import ManageSchedule from "../Components/ViewTeacher/ManageSchedule"
 import MyStudentSay from "../Components/ViewTeacher/MyStudentSay"
 import ProfileHeader from "../Components/ViewTeacher/ProfileHeader"
 import ResumeMe from "../Components/ViewTeacher/ResumeMe"
 import Verifications from "../Components/ViewTeacher/Verifications"
+import { useTeacherProfile } from "../Hooks/useTeacherProfile"
+import { BASE_URL } from "../Utils/Apis"
 
 const ViewTeacher = () => {
+  const { data } = useTeacherProfile();
+  
   return <>
 
     <section className="ProfileStudent-section bg-[#F9FBFC]">
@@ -15,7 +20,11 @@ const ViewTeacher = () => {
        <div className="flex px-60 mt-12 justify-between items-start gap-16">
           <div className="right-content">
             
-            <AboutMe/>
+            {
+              data?.data?.data?.introVideoPath !== BASE_URL
+                ? <AboutTeacher />
+                : <AboutMe />
+            }
 
             <Verifications/>
 
