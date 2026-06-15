f7fffe0e14081c65f2da5f4246b1c240bb4531f6
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-15 19:47:56 +0300

    Update ProfileCompletion Component Page

--- src/Features/Profile/Components/ViewTeacher/ProfileCompletion.tsx
+++ src/Features/Profile/Components/ViewTeacher/ProfileCompletion.tsx
@@ -1,27 +1,34 @@
+import { roleToAuth } from "../../../../Utils/Constant";
+import { useTeacherProfile } from "../../Hooks/useTeacherProfile";
 
 const ProfileCompletion = () => {
-  return <>
-  
-    <section className="ProfileCompletion-section">
+  const { data } = useTeacherProfile();
+
+  const isTeacher = roleToAuth?.includes("Teacher") ? true : false;
+  if (!isTeacher) {
+    return;
+  }
+
+  return (
+    <>
+      <section className="ProfileCompletion-section">
         <p className="font-medium text-[16px] mb-2">
-            Profile Completion: 15%
+          Profile Completion: {data?.data?.data?.completionPercentage || 0}%
         </p>
 
         <div className="flex items-center">
-            <div className="w-[541px] mr-2.5 rounded-full bg-[#E8EAED] h-3 overflow-hidden">
-                <div
-                className="bg-[#525FE1] rounded-full h-3"
-                style={{
-                    width: `15%`,
-                }}
-                />
-            </div>
+          <div className="w-[541px] mr-2.5 rounded-full bg-[#E8EAED] h-3 overflow-hidden">
+            <div
+              className="bg-[#525FE1] rounded-full h-3"
+              style={{
+                width: `${data?.data?.data?.completionPercentage || 0}%`,
+              }}
+            />
+          </div>
         </div>
+      </section>
+    </>
+  );
+};
 
-    </section>
-  
-  
-  </>
-}
-
-export default ProfileCompletion
+export default ProfileCompletion;
