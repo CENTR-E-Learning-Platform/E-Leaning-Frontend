a4aee25d5b916c4178dab40c767a97db64e06c5b
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-15 20:12:06 +0300

    Update Bio And VideoSchema

--- src/Features/Profile/Validation/BioAndVideoSchema.ts
+++ src/Features/Profile/Validation/BioAndVideoSchema.ts
@@ -1,15 +1,15 @@
 import * as Yup from "yup";
 
 export const IntroVideoAndBioSchema = Yup.object({
-  bio: Yup.string().required("Profile overview is required"),
+  bio: Yup.string(),
   video: Yup.mixed()
-  .required("Video is required")
-  .test(
-    "fileSize",
-    "Video size must not exceed 30 MB",
-    (value) => {
-      if (!value) return false;
-      return (value as File).size <= 30 * 1024 * 1024;
-    }
-  ),
+    .nullable()
+    .test(
+      "fileSize",
+      "Video size must not exceed 30 MB",
+      (value) => {
+        if (!value) return true;
+        return (value as File).size <= 30 * 1024 * 1024;
+      }
+    ),
 });
