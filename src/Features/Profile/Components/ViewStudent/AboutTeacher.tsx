88c3ef47d92eec3eba18061b879b67a34fb90337
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-17 04:46:02 +0300

    feat: add profile view components for students and teachers including statistics, bio management, and profile layout pages

--- src/Features/Profile/Components/ViewStudent/AboutTeacher.tsx
+++ src/Features/Profile/Components/ViewStudent/AboutTeacher.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import { useTeacherProfile } from "../../Hooks/useTeacherProfile";
+import { useStudentProfileContext } from "../../Contexts/StudentProfileContext";
+import { BASE_URL } from "../../Utils/Apis";
 
 const AboutTeacher = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [showButton, setShowButton] = useState(false);
-  const {data} = useTeacherProfile();
+  const { teacherProfile } = useStudentProfileContext();
   const textRef = useRef<HTMLParagraphElement>(null);
+  console.log(teacherProfile)
 
   useEffect(() => {
     const el = textRef.current;
@@ -14,7 +16,7 @@ const AboutTeacher = () => {
         setShowButton(true);
       }
     }
-  }, [data]);
+  }, [teacherProfile]);
 
   return (
     <>
@@ -23,18 +25,24 @@ const AboutTeacher = () => {
           About me
         </h2>
 
-        <iframe
-          width="400"
-          height="219"
-          src={data?.data?.data?.introVideoPath}
-          title="video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          className="w-[400px] h-[219px] rounded-[4px] mb-6"
-        ></iframe>
+        {teacherProfile?.introVideoPath &&
+          teacherProfile.introVideoPath !== BASE_URL && (
+            <iframe
+              width="400"
+              height="219"
+              src={`${BASE_URL}${teacherProfile.introVideoPath}`}
+              title="video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className="w-[400px] h-[219px] rounded-[4px] mb-6"
+            ></iframe>
+          )}
 
-        <p ref={textRef} className={`text-[16px] font-medium font-Poppins text-[#2A2D34] ${!isExpanded ? "line-clamp-2" : ""}`}>
-          {data?.data?.data.bio}
+        <p
+          ref={textRef}
+          className={`text-[16px] font-medium font-Poppins text-[#2A2D34] ${!isExpanded ? "line-clamp-2" : ""}`}
+        >
+          {teacherProfile?.bio ?? "No bio available."}
         </p>
         {showButton && (
           <button
@@ -44,7 +52,6 @@ const AboutTeacher = () => {
             {isExpanded ? "Read less" : "Read more"}
           </button>
         )}
-
       </section>
     </>
   );
