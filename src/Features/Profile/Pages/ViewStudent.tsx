262f54351862015a616e8ab8e0fa06b69f87c0a4
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-17 04:46:46 +0300

    feat: implement student and teacher profile view pages with context providers

--- src/Features/Profile/Pages/ViewStudent.tsx
+++ src/Features/Profile/Pages/ViewStudent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProfileHeader from "../Components/ViewStudent/ProfileHeader";
 import TeacherStats from "../Components/ViewStudent/TeacherStats";
 import AboutTeacher from "../Components/ViewStudent/AboutTeacher";
@@ -6,32 +6,72 @@ import TeactherVerifications from "../Components/ViewStudent/TeactherVerificatio
 import StudentsSay from "../Components/ViewStudent/StudentsSay";
 import TeacherResume from "../Components/ViewStudent/TeacherResume";
 import AvailableClasses from "../Components/ViewStudent/AvailableClasses";
+import {
+  StudentProfileContext,
+  type TeacherProfileData,
+} from "../Contexts/StudentProfileContext";
+import { useStudentProfile } from "../Hooks/useViewStudentProfile";
 
 const ViewStudent = () => {
+  const { mutate, isPending } = useStudentProfile();
+  const [teacherProfile, setTeacherProfile] =
+    useState<TeacherProfileData | null>(null);
+
+  useEffect(() => {
+    const cached = localStorage.getItem("viewStudentProfileData");
+    if (cached) {
+      try {
+        setTeacherProfile(JSON.parse(cached));
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    const teacherId = localStorage.getItem("viewStudentTeacherId");
+    if (teacherId) {
+      mutate(teacherId, {
+        onSuccess: (response) => {
+          const profileData = response?.data?.data ?? null;
+          if (profileData) {
+            setTeacherProfile(profileData);
+            localStorage.setItem(
+              "viewStudentProfileData",
+              JSON.stringify(profileData)
+            );
+          }
+        },
+      });
+    }
+  }, []);
+
   return (
-    <React.Fragment>
-      <section className="MainProfileTeacther">
-        <ProfileHeader />
+    <StudentProfileContext.Provider
+      value={{ teacherProfile, isLoading: isPending }}
+    >
+      <React.Fragment>
+        <section className="MainProfileTeacther">
+          <ProfileHeader />
 
-        <div className="flex px-60 mt-30 justify-between items-start gap-16">
-          <div className="right-content">
-            <TeacherStats />
+          <div className="flex px-60 mt-30 justify-between items-start gap-16">
+            <div className="right-content">
+              <TeacherStats />
 
-            <AboutTeacher />
+              <AboutTeacher />
 
-            <TeactherVerifications />
+              <TeactherVerifications />
 
-            <StudentsSay />
+              <StudentsSay />
 
-            <TeacherResume />
+              <TeacherResume />
+            </div>
+
+            <div className="left-content sticky top-4">
+              <AvailableClasses />
+            </div>
           </div>
-
-          <div className="left-content sticky top-4">
-            <AvailableClasses />
-          </div>
-        </div>
-      </section>
-    </React.Fragment>
+        </section>
+      </React.Fragment>
+    </StudentProfileContext.Provider>
   );
 };
 
