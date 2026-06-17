b355a84ab056a94dd9aa2670ea3b6ede4d48a4fa
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-17 04:46:23 +0300

    feat: implement teacher profile view and editing functionality with dedicated context and hooks

--- src/Features/Profile/Components/ViewStudent/TeacherStats.tsx
+++ src/Features/Profile/Components/ViewStudent/TeacherStats.tsx
@@ -1,58 +1,45 @@
-
 import StartTeacher from "../../../../../src/assets/icons/StartTeacher.svg";
 import ButtonStatus from "./ButtonStatus";
+import { useStudentProfileContext } from "../../Contexts/StudentProfileContext";
 
 const TeacherStats = () => {
-  return <>
-    
-    <section className="TeacherStats-section mb-12">
+  const { teacherProfile } = useStudentProfileContext();
+  console.log("teacherProfile", teacherProfile);
+  const rating = teacherProfile?.rate ?? 0;
+  const reviews = teacherProfile?.numberOfReviews ?? 0;
+  const yearsExp = teacherProfile?.yearsExperience ?? 0;
+  const completedClasses = teacherProfile?.completedClasses ?? 0;
 
-      <h2 className="text-[24px] mb-4 text-[#2A2D34] font-Poppins font-bold">
-        Teacher stats
-      </h2>
-      
-      <div className="TeacherStats-content flex justify-between items-start gap-2 w-[423px]">
-        
-        <ButtonStatus imgClass="" icon={StartTeacher} rating="4.9" NumberOfReviews="(120 Review)"/>
-        <ButtonStatus imgClass="hidden" icon={""} rating="3+" NumberOfReviews="Years Experience"/>
-        <ButtonStatus imgClass="hidden" icon={""} rating="174" NumberOfReviews="Completed classes"/>
+  return (
+    <>
+      <section className="TeacherStats-section mb-12">
+        <h2 className="text-[24px] mb-4 text-[#2A2D34] font-Poppins font-bold">
+          Teacher stats
+        </h2>
 
-      </div>
-    </section>
-  </>
-}
-
-export default TeacherStats
-
-
-
-
-
-
-
-
-
-
-/**
- * 
- * <div className="bg-[#C8CCF44D] px-4 py-2 rounded-[4px] h-[60px]">
-          <div className="w-[50px] flex gap-1 justify-between items-center">
-
-            <img src={StartTeacher} alt="StartTeacher" />
-
-            <p className="text-[18px] font-Poppins font-semibold text-[#4152FB]">
-              4.8
-            </p>
-
-          </div>
-
-          <p className="text-[#4152FB] font-semibold text-[12px]">
-            502 reviews
-          </p>
-
+        <div className="TeacherStats-content flex justify-between items-start gap-2 w-[423px]">
+          <ButtonStatus
+            imgClass=""
+            icon={StartTeacher}
+            rating={String(rating)}
+            NumberOfReviews={`(${reviews} Review)`}
+          />
+          <ButtonStatus
+            imgClass="hidden"
+            icon={""}
+            rating={`${yearsExp}+`}
+            NumberOfReviews="Years Experience"
+          />
+          <ButtonStatus
+            imgClass="hidden"
+            icon={""}
+            rating={String(completedClasses)}
+            NumberOfReviews="Completed classes"
+          />
         </div>
+      </section>
+    </>
+  );
+};
 
-        
- * 
- * 
- */
+export default TeacherStats;
