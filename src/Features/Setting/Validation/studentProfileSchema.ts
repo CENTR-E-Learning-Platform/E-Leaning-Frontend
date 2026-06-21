fb9613246ee02a37c8b41604b5b58b5fb4978407
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-21 06:08:10 +0300

    Change the validation in StudentProfileSchema

--- src/Features/Setting/Validation/studentProfileSchema.ts
+++ src/Features/Setting/Validation/studentProfileSchema.ts
@@ -1,8 +1,6 @@
 import * as Yup from "yup";
 import { egyptPhoneRegex } from "../Constant/egyptPhoneRegex";
 
-
-
 export const studentProfileSchema = Yup.object({
   firstName: Yup.string()
     .min(3, "First name must be at least 3 characters")
@@ -11,20 +9,18 @@ export const studentProfileSchema = Yup.object({
     .notRequired(),
 
   secondName: Yup.string()
+    .transform((value) => (value === "" ? undefined : value))
     .min(3, "Last name must be at least 3 characters")
     .max(30, "Last name must be less than 30 characters")
-    .matches(/^[a-zA-Z]+$/, "Last name must contain only letters")
     .notRequired(),
-    
 
   email: Yup.string()
     .email("Email is invalid")
     .matches(
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      "Please enter a valid email address"
+      "Please enter a valid email address",
     ),
 
-    
   phoneNum: Yup.string()
     .notRequired()
     .test(
@@ -38,22 +34,22 @@ export const studentProfileSchema = Yup.object({
         if (value && parentPhone && value === parentPhone) return false;
 
         return true;
-      }
+      },
     ),
 
-parentPhoneNumber: Yup.string()
-  .notRequired()
-  .test(
-    "valid-and-different",
-    "Parent phone number must be valid and different from student phone number",
-    function (value) {
-      const studentPhone = this.parent.phoneNum;
+  parentPhoneNumber: Yup.string()
+    .notRequired()
+    .test(
+      "valid-and-different",
+      "Parent phone number must be valid and different from student phone number",
+      function (value) {
+        const studentPhone = this.parent.phoneNum;
 
-      if (!value) return true;
-      if (!egyptPhoneRegex.test(value)) return false;
-      if (value && studentPhone && value === studentPhone) return false;
+        if (!value) return true;
+        if (!egyptPhoneRegex.test(value)) return false;
+        if (value && studentPhone && value === studentPhone) return false;
 
-      return true;
-    }
-  ),
+        return true;
+      },
+    ),
 });
