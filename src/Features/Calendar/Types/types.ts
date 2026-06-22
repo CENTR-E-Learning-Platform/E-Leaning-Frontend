dcff203f8f6ef3bbb2c9347a21491f6e16198f36
Abdelrhmancs <ght7234@gmail.com>
2026-06-22 04:04:00 +0300

    feat: add Session interface to define session properties for calendar features

---
+++ src/Features/Calendar/Types/types.ts
@@ -0,0 +1,10 @@
+export interface Session {
+    id: string;
+    price: number;
+    title: string;
+    startTime: string;
+    durationMinutes: number;
+    grade: number;
+    reminder: string;
+    description: string;
+}
