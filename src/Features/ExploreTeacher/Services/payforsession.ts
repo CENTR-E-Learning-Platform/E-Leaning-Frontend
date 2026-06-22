df5f3114e7e9e10e51e3e3e39085e35327b27292
Abdelrhmancs <ght7234@gmail.com>
2026-06-22 04:05:17 +0300

    feat: update payforsession function to accept roomName parameter

--- src/Features/ExploreTeacher/Services/payforsession.ts
+++ src/Features/ExploreTeacher/Services/payforsession.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
-import {  PAY_FOR_SESSION_API } from "../Utils/api";
-
-export const payforsession = async () =>{
+import { PAY_FOR_SESSION_API } from "../Utils/api";
+export const payforsession = async (roomName: string) => {
     const token = localStorage.getItem("token") || "";
     console.log(token)
     return await axios.post(
         PAY_FOR_SESSION_API,
         {
-            roomName: "session_14227585-1836-401f-b7d6-08dec412f497_1"
+            roomName: roomName
         },
         {
             headers: {
