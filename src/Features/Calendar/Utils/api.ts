41df73dd32d6d3c6e1e396b69cfadf9e59c3350e
Abdelrhmancs <ght7234@gmail.com>
2026-06-23 17:46:38 +0300

    feat: add UPDATE_SESSION_SERIES_API constant for session series updates

--- src/Features/Calendar/Utils/api.ts
+++ src/Features/Calendar/Utils/api.ts
@@ -4,4 +4,5 @@ const BASE_URL = "https://localhost:7251";
 const TEACHER_QUIZ = `${BASE_URL}/api/Calendar/get-teacher-quizzes-calendar`;
 const STUDENT_QUIZ = `${BASE_URL}/api/Calendar/get-student-quizzes-calendar`;
 const UPDATE_SESSION_API = `${BASE_URL}/api/Room/UpdateSession`;
-export { TEACHER_CLASSES, STUDENT_CLASSES, BASE_URL, TEACHER_QUIZ, STUDENT_QUIZ, UPDATE_SESSION_API };
+const UPDATE_SESSION_SERIES_API = `${BASE_URL}/api/Room/UpdateSeries`;
+export { TEACHER_CLASSES, STUDENT_CLASSES, BASE_URL, TEACHER_QUIZ, STUDENT_QUIZ, UPDATE_SESSION_API, UPDATE_SESSION_SERIES_API };
