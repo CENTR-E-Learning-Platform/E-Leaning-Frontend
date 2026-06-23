cc13d29ccd683051583b1bf7efbe4833e266465b
Abdelrhmancs <ght7234@gmail.com>
2026-06-23 17:46:25 +0300

    feat: add UpdateSessionSeries function to handle session series updates

--- src/Features/Calendar/Services/updateSession.ts
+++ src/Features/Calendar/Services/updateSession.ts
@@ -1,9 +1,23 @@
 import axios from "axios"
-import { UPDATE_SESSION_API } from "../Utils/api"
+import { UPDATE_SESSION_API, UPDATE_SESSION_SERIES_API } from "../Utils/api"
+
 export const UpdateSession = async (data: any) => {
     const token = localStorage.getItem("token") || "";
     return axios.patch(
         UPDATE_SESSION_API,
+        data,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        }
+    )
+}
+
+export const UpdateSessionSeries = async (data: any) => {
+    const token = localStorage.getItem("token") || "";
+    return axios.patch(
+        UPDATE_SESSION_SERIES_API,
         data,
         {
             headers: {
