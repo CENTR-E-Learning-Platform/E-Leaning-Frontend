237db6383748253debdcdd0495fa983a3a484859
abdoo-ahmed <abdoahmed8959@gmail.com>
2026-06-25 07:32:40 +0300

    Update api.ts

--- src/Features/ExploreTeacher/Utils/api.ts
+++ src/Features/ExploreTeacher/Utils/api.ts
@@ -1,6 +1,6 @@
-const API_BASE_URL = "https://localhost:7251";
-const SEARCH_TEACHER_API = `${API_BASE_URL}/Search`;
-const FILTER_TEACHER_API = `${API_BASE_URL}/Filter`;
-const ALL_TEACHER_API = `${API_BASE_URL}/GetAllTeachers`;
-const PAY_FOR_SESSION_API = `${API_BASE_URL}/api/SessionPayment/PayForSession`;
-export {SEARCH_TEACHER_API, FILTER_TEACHER_API, ALL_TEACHER_API, PAY_FOR_SESSION_API};
+const BASE_URL = "https://localhost:7251";
+const SEARCH_TEACHER_API = `${BASE_URL}/Search`;
+const FILTER_TEACHER_API = `${BASE_URL}/Filter`;
+const ALL_TEACHER_API = `${BASE_URL}/GetAllTeachers`;
+const PAY_FOR_SESSION_API = `${BASE_URL}/api/SessionPayment/PayForSession`;
+export {SEARCH_TEACHER_API, FILTER_TEACHER_API, ALL_TEACHER_API, PAY_FOR_SESSION_API , BASE_URL};
