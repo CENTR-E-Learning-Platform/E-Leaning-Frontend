7569814e642bc557bef8eccf7af4a186805ed5ca
Abdelrhmancs <ght7234@gmail.com>
2026-07-12 00:13:28 +0300

    feat: enhance fetchClasses function to support date range and loading state

--- src/Features/Calendar/Hooks/useGetAllClasses.ts
+++ src/Features/Calendar/Hooks/useGetAllClasses.ts
@@ -1,37 +1,42 @@
-import { useEffect, useCallback } from "react";
+import { useCallback, useState } from "react";
 import { getAllTeacherClasses } from "../Services/getAllTeacherClasses";
 import { useCalendar } from "../Contexts/CalendarContext";
 import { roleToAuth } from "../../../Utils/Constant";
 import { getAllStudentClasses } from "../Services/getAllStudentClasses";
+import { startOfMonth, endOfMonth, format } from "date-fns";
 
 export const useGetAllClasses = () => {
   const { setClass } = useCalendar();
+  const [isLoading, setIsLoading] = useState(false);
 
   const isTeacher = !!roleToAuth?.includes("Teacher");
 
-  const fetchClasses = useCallback(async () => {
-    try {
-      let fetchedData;
+  const fetchClasses = useCallback(
+    async (referenceDate: Date = new Date()) => {
+      const start = format(startOfMonth(referenceDate), "yyyy-MM-dd");
+      const end = format(endOfMonth(referenceDate), "yyyy-MM-dd");
 
-      if (isTeacher) {
-        const response = await getAllTeacherClasses();
-        fetchedData = response.data.data;
-      } else {
-        const response = await getAllStudentClasses();
-        fetchedData = response.data.data;
+      setIsLoading(true);
+      try {
+        let fetchedData;
+
+        if (isTeacher) {
+          const response = await getAllTeacherClasses(start, end);
+          fetchedData = response.data.data;
+        } else {
+          const response = await getAllStudentClasses(start, end);
+          fetchedData = response.data.data;
+        }
+
+        setClass(fetchedData);
+      } catch (error) {
+        console.error("Error fetching classes:", error);
+      } finally {
+        setIsLoading(false);
       }
+    },
+    [setClass, isTeacher]
+  );
 
-      setClass(fetchedData);
-      console.log(fetchedData);
-
-    } catch (error) {
-      console.error("Error fetching classes:", error);
-    }
-  }, [setClass, isTeacher]); 
-
-  useEffect(() => {
-    fetchClasses();
-  }, [fetchClasses]);
-
-  return { fetchClasses };
+  return { fetchClasses, isLoading };
 };
