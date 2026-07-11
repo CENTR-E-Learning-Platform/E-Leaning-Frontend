55ffdfddeeccea4778a9c87bef68046cd1d33785
Abdelrhmancs <ght7234@gmail.com>
2026-07-12 00:14:00 +0300

    feat: update getAllTeacherClasses function to accept date range parameters

--- src/Features/Calendar/Services/getAllTeacherClasses.ts
+++ src/Features/Calendar/Services/getAllTeacherClasses.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { TEACHER_CLASSES } from "../Utils/api";
 import { BASE_URL } from "../Utils/api";
-export const getAllTeacherClasses = async() => {
+export const getAllTeacherClasses = async(start:any , end:any) => {
     const token  = localStorage.getItem("token");
-    return await axios.get(`${BASE_URL}/${TEACHER_CLASSES}` ,
+    return await axios.get(`${BASE_URL}/${TEACHER_CLASSES}?Start=${start}&End=${end}` ,
     {
         headers: {
             Authorization: `Bearer ${token}`
