c6114be46c6475b5fe473d5f3859dda6a0407ffe
Abdelrhmancs <ght7234@gmail.com>
2026-07-20 19:25:57 +0300

    feat: enhance EmojiPicker component with styling and layout improvements

--- src/Features/Messages/Components/Message/EmojiPicker.tsx
+++ src/Features/Messages/Components/Message/EmojiPicker.tsx
@@ -7,10 +7,16 @@ type Props = {
 
 const EmojiPicker = ({ onSelect }: Props) => {
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 bottom-[10px] z-50">
+    <div className="absolute left-1/2 -translate-x-1/2 bottom-[10px] z-50 bg-white rounded-lg shadow-xl border border-gray-100">
       <Picker
         data={data}
         onEmojiSelect={(emoji: any) => onSelect(emoji.native)}
+        theme="light"
+        previewPosition="none"
+        emojiSize={20}
+        emojiButtonSize={28}
+        perLine={7}
+        maxFrequentRows={1}
       />
     </div>
   );
