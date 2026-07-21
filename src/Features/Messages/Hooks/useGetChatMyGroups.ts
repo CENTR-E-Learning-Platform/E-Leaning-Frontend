ebe20cd42c0405d339fc01183fcb57a7176ea85b
Abdelrhmancs <ght7234@gmail.com>
2026-07-21 17:57:07 +0300

    feat: add loading state to useGetChatMyGroups hook for improved UI feedback

--- src/Features/Messages/Hooks/useGetChatMyGroups.ts
+++ src/Features/Messages/Hooks/useGetChatMyGroups.ts
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getChatMyGroups } from "../Services/getChatMyGroups";
 
 export const useGetChatMyGroups = () => {
-  const { data, refetch :refetchChatMyGroups } = useQuery({
+  const { data, refetch: refetchChatMyGroups, isLoading } = useQuery({
     queryKey: ["GetChatMyGroup"],
     queryFn: getChatMyGroups,
     refetchOnWindowFocus: true,
     refetchOnMount: true,
-    refetchOnReconnect: true, 
+    refetchOnReconnect: true,
     staleTime: 30000
   });
 
-  return { data, refetchChatMyGroups };
+  return { data, refetchChatMyGroups, isLoading };
 };
